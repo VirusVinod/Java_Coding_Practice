aa9eb73ec95a6cfa62c478d5bd9dcf2ee06c83a8
VinodSingh680 <vinodkumarsingh680@gmail.com>
2024-12-29 19:14:16 +0530

    Null Argument

---
+++ src/main/java/CodingPactice/NullArgument.java
@@ -0,0 +1,21 @@
+package CodingPactice;
+
+//Pass null argument with method overloading of String and Object types
+
+public class NullArgument {
+
+	public static void main(String[] args) {
+		test(null);
+	}
+
+	public static void test(Object o) {
+		System.out.println("Object Argument");
+
+	}
+
+	public static void test(String s) {
+		System.out.println("String Argument");
+
+	}
+
+}

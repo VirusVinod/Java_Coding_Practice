8906be0663debeeff507eba146d1e29756a44dd5
VinodSingh680 <vinodkumarsingh680@gmail.com>
2024-12-31 13:12:29 +0530

    comment

--- src/test/java/Selenium/SwiggyHomePage.java
+++ src/test/java/Selenium/SwiggyHomePage.java
@@ -27,10 +27,8 @@ public class SwiggyHomePage {
 		String URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Biryani.png";
 		List<WebElement> items = driver.findElements(By.cssSelector(".sc-kMkxaj.bzknTh"));
 		for (WebElement item : items) {
-			System.out.print(item);
 			WebElement img = item.findElement(By.tagName("img"));
 			String imageUrl = img.getDomProperty("src");
-			System.out.print("Image URL: " + imageUrl);
 			if (imageUrl.equals(URL)) {
 				item.click();			}
 		}

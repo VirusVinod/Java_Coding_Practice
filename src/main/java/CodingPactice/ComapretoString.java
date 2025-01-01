27d958cf48ec4c44277beac673c662255c9ee266
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-01 11:44:14 +0530

    Comapre to String

---
+++ src/main/java/CodingPactice/ComapretoString.java
@@ -0,0 +1,16 @@
+package CodingPactice;
+
+public class ComapretoString {
+
+	public static void main(String[] args) {
+
+		String s = "Test 1";
+		String S2 = "Test 1";
+		
+		System.out.println(s.equals(S2));
+				
+		
+
+	}
+
+}

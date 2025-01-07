1982fb331eca4fd42bef70f8dbdb031a1e03e28e
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-07 11:07:39 +0530

    FindstheLowestAge

---
+++ src/main/java/ArrayConcept/FindstheLowestAge.java
@@ -0,0 +1,20 @@
+package ArrayConcept;
+
+public class FindstheLowestAge {
+
+	public static void main(String[] args) {
+		int ages[] = { 20, 22, 18, 35, 48, 26, 87, 70, 9 };
+
+		int lowestAge = ages[0];
+
+		for (int age : ages) {
+			if (lowestAge > age) {
+				lowestAge = age;
+			}
+		}
+
+		System.out.println("The lowest age in the array is: " + lowestAge);
+
+	}
+
+}

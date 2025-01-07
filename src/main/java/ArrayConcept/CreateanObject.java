b36f298e7dabaf8ad65428c0b8523a88ca0b6579
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-07 11:27:03 +0530

    Create an object

---
+++ src/main/java/ArrayConcept/CreateanObject.java
@@ -0,0 +1,14 @@
+package ArrayConcept;
+
+public class CreateanObject {
+	
+	int array[]= {1,3,4,5,6,7,8};
+
+	public static void main(String[] args) {
+		
+		CreateanObject ob = new CreateanObject();
+		System.out.println(ob.array[1]);
+
+	}
+
+}

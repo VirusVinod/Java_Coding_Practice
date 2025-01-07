589902e3e9ff4bb8a8f1b3502f2824d83422227e
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-07 11:00:58 +0530

    Array Concept

---
+++ src/main/java/ArrayConcept/CarsArray.java
@@ -0,0 +1,13 @@
+package ArrayConcept;
+
+public class CarsArray {
+
+	public static void main(String[] args) {
+		String[] cars = {"Volvo","BMW","Ford","Mazda"};
+		int i;
+		
+		for(i=0;i<=cars.length;i++) {
+			System.out.println(cars[i]);
+		}
+	}
+}

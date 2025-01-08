5dd4331bebdb68550eb7cc23ebe1d9e4c6552109
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-08 09:28:15 +0530

    Linked List

---
+++ src/main/java/ArrayConcept/List_Linked.java
@@ -0,0 +1,17 @@
+package ArrayConcept;
+
+import java.util.LinkedList;
+
+public class List_Linked {
+
+	public static void main(String[] args) {
+		LinkedList<String> cars = new LinkedList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    System.out.println(cars);
+
+	}
+
+}

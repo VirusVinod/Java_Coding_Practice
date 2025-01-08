3cf512b489dea5599e12f2cc8b6df698f4405b58
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-08 09:26:06 +0530

    ArrayList

---
+++ src/main/java/ArrayConcept/ListArray.java
@@ -0,0 +1,17 @@
+package ArrayConcept;
+import java.util.ArrayList;
+
+public class ListArray {
+
+	public static void main(String[] args) {
+		ArrayList<String> cars = new ArrayList<String>();
+		    cars.add("Volvo");
+		    cars.add("BMW");
+		    cars.add("Ford");
+
+		    cars.add(0, "Mazda"); // Insert element at the beginning of the list (0)
+
+		    System.out.println(cars);
+	}
+
+}

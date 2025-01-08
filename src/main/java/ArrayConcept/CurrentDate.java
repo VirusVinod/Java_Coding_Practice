b04b6dc69cafb693e82c2ae5acf09ed0d8ccea1c
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-08 09:31:56 +0530

    Current Date

---
+++ src/main/java/ArrayConcept/CurrentDate.java
@@ -0,0 +1,13 @@
+package ArrayConcept;
+
+import java.time.LocalDate;
+
+public class CurrentDate {
+
+	public static void main(String[] args) {
+		LocalDate Ldate = LocalDate.now();
+		System.out.println(Ldate);
+
+	}
+
+}

e74b754f1668c44d824e8a6030443bb054a9e9d5
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-09 20:15:21 +0530

    if elese condition

---
+++ src/main/java/ArrayConcept/ifelseCondition.java
@@ -0,0 +1,20 @@
+package ArrayConcept;
+
+import java.util.Scanner;
+
+public class ifelseCondition {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		System.out.println("Your age is: " + age);
+		
+		if(age>18) {
+			System.out.println("Adult");
+		}else {
+			System.out.println("Not adult");
+		}
+	}
+		
+
+}

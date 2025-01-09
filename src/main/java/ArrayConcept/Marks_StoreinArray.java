39bd01227ea3c63a964ee6a6f08b7ca1ccd356f2
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-09 19:47:24 +0530

    Store marks in array

---
+++ src/main/java/ArrayConcept/Marks_StoreinArray.java
@@ -0,0 +1,13 @@
+package ArrayConcept;
+
+public class Marks_StoreinArray {
+
+	public static void main(String[] args) {
+		int[] marks = new int[20];
+		marks[0]=97;
+		marks[1]=23;
+		marks[2]=68;
+		System.out.println(marks[1]);
+	}
+
+}

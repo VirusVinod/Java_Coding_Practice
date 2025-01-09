b981900203a49b3a97dcb44cd0aa0a07b704129b
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-09 19:50:51 +0530

    Store marks in array

--- src/main/java/ArrayConcept/Marks_StoreinArray.java
+++ src/main/java/ArrayConcept/Marks_StoreinArray.java
@@ -4,10 +4,14 @@ public class Marks_StoreinArray {
 
 	public static void main(String[] args) {
 		int[] marks = new int[20];
-		marks[0]=97;
-		marks[1]=23;
-		marks[2]=68;
-		System.out.println(marks[1]);
+		marks[0] = 97;
+		marks[1] = 23;
+		marks[2] = 68;
+//		System.out.println(marks[1]);
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(marks[i]);
+		}
 	}
 
 }

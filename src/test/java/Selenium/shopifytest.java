fd287c2bc58762f9548118c91ba0e9d671b15e0f
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-09 11:44:48 +0530

    shopify

---
+++ src/test/java/Selenium/shopifytest.java
@@ -0,0 +1,26 @@
+package Selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class shopifytest {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://automation108.myshopify.com/");
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		
+		
+		WebElement EnterStorePassword =driver.findElement(By.id("password"));
+		EnterStorePassword.sendKeys("visitor");
+		
+		WebElement enter =driver.findElement(By.xpath("//button[@type='submit']"));
+		enter.click();
+	}
+}

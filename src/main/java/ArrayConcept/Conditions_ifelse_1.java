4da3274d4db6689d2b130c9aae3ec225268ac662
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-10 19:51:57 +0530

    condition else if

---
+++ src/main/java/ArrayConcept/Conditions_ifelse_1.java
@@ -0,0 +1,21 @@
+package ArrayConcept;
+
+import java.util.Scanner;
+
+public class Conditions_ifelse_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b= sc.nextInt();
+		
+		if(a==b) {
+			System.out.println("Equal");
+		}else if (a>b){
+			System.out.println("a is greater then");
+		}else {
+			System.out.println("b is lasser ");
+		}
+	}
+
+}

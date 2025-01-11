74088c3829eec3b03da482ebfab445ce9c871216
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-11 19:22:43 +0530

    java Regex

---
+++ src/main/java/ArrayConcept/JavaRegex.java
@@ -0,0 +1,31 @@
+package ArrayConcept;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+
+	String pattern = "^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\.){3}" + "(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$";
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		JavaRegex myRegex = new JavaRegex();
+
+		while (scanner.hasNext()) {
+			String IP = scanner.next();
+			System.out.println(IP.matches(myRegex.pattern));
+		}
+
+		scanner.close();
+
+	}
+
+}
+
+//Sample Input:
+//Copy code
+//000.12.12.034
+//121.234.12.12
+//23.45.12.56
+//00.12.123.123123.123
+//122.23
+//Hello.IP

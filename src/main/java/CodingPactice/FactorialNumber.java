b94e56908efbc65b1b5389973ef0c0e708abcc1b
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-12 18:18:55 +0530

    With recursive

--- src/main/java/CodingPactice/FactorialNumber.java
+++ src/main/java/CodingPactice/FactorialNumber.java
@@ -7,17 +7,27 @@ public class FactorialNumber {
 		int i;
 		if (num == 0)
 			return 1;
+
 		for (i = 1; i <= num; i++) {
-			fact = fact * i; 
+			fact = fact * i;
 		}
 		return fact;
+	}
 
+	// with recursive function: a function is calling it self
+	public static int fact(int num) {
+		if (num == 0)
+			return 1;
+		else
+			return (num * fact(num - 1));
 	}
 
 	public static void main(String[] args) {
 		System.out.println(Factorial(4));
 		System.out.println(Factorial(1));
 		System.out.println(Factorial(0));
+		System.out.println(fact(3));
+
 	}
 
 }

92e4a6fa0a1134b79acdde3189fd414802b68705
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-12 18:08:10 +0530

    FactorialNumber

--- src/main/java/CodingPactice/FactorialNumber.java
+++ src/main/java/CodingPactice/FactorialNumber.java
@@ -5,8 +5,10 @@ public class FactorialNumber {
 	public static int Factorial(int num) {
 		int fact = 1;
 		int i;
+		if (num == 0)
+			return 1;
 		for (i = 1; i <= num; i++) {
-			fact = fact * i;
+			fact = fact * i; 
 		}
 		return fact;
 
@@ -14,6 +16,8 @@ public class FactorialNumber {
 
 	public static void main(String[] args) {
 		System.out.println(Factorial(4));
+		System.out.println(Factorial(1));
+		System.out.println(Factorial(0));
 	}
 
 }

dc95c2dff7e00951d60acea73b0befd03384e334
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-13 20:22:10 +0530

    Armstrong Number

--- src/main/java/CodingPactice/ArmstrongNumber.java
+++ src/main/java/CodingPactice/ArmstrongNumber.java
@@ -2,9 +2,28 @@ package CodingPactice;
 
 public class ArmstrongNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void isArmstrongNumber(int num) {
+		int cube = 0;
+		int r;
+		int t;
 
+		t = num;
+		while (num > 0) {
+			r = num % 10;
+			num = num / 10;
+			cube = cube + (r * r * r);
+
+		}
+		if (t == cube) {
+			System.out.println(" This is Armstrong Number");
+		} else {
+			System.out.println(" This is Armstrong not Number");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		isArmstrongNumber(1533);
 	}
 
 }

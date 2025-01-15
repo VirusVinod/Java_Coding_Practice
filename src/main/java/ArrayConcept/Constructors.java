b28bf3923665bf558ca7b687c16f60e21413344d
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-15 19:47:04 +0530

    Constructors

---
+++ src/main/java/ArrayConcept/Constructors.java
@@ -0,0 +1,18 @@
+package ArrayConcept;
+
+public class Constructors {
+	public static class Main {
+		  int x;
+
+		  public Main(int y) {
+		    x = y;
+		  }
+
+		  public static void main(String[] args) {
+		    Main myObj = new Main(5);
+		    System.out.println(myObj.x);
+		  }
+		}
+
+
+}

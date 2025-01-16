24616a784e8ce3ba63085efc7d44c78f0ac8fe97
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-16 20:41:04 +0530

    Inner Classes

---
+++ src/main/java/ArrayConcept/InnerClasses.java
@@ -0,0 +1,17 @@
+package ArrayConcept;
+
+public class InnerClasses {
+	int x = 10;
+
+	class InnerClass {
+		int y = 5;
+
+	}
+
+	public static void main(String[] args) {
+		InnerClasses myOuter = new InnerClasses();
+		InnerClasses.InnerClass myInner = myOuter.new InnerClass();
+		System.out.println(myInner.y + myOuter.x);
+	}
+
+}

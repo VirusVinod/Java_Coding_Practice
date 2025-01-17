72d286643a606f8da5ba46987044fef06669c9b1
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-17 09:49:45 +0530

    polymorphism

--- src/main/java/ArrayConcept/Polymorphism.java
+++ src/main/java/ArrayConcept/Polymorphism.java
@@ -9,13 +9,13 @@ public class Polymorphism {
 
 	class Pig extends Animal {
 		public void animalSound() {
-			System.out.println("The pig says: wee wee");
+			System.out.println("The pig says: wee wee wee");
 		}
 	}
 
 	class Dog extends Animal {
 		public void animalSound() {
-			System.out.println("The dog says: bow wow");
+			System.out.println("The dog says: bow bow wow");
 		}
 	}
 

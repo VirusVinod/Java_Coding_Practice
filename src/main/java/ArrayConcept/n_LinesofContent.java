6e36a43c1acdc978aab28e26984c7f1dfdf14db8
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-19 10:24:09 +0530

    Read some unknown  lines of input

---
+++ src/main/java/ArrayConcept/n_LinesofContent.java
@@ -0,0 +1,25 @@
+package ArrayConcept;
+
+import java.util.Scanner;
+
+public class n_LinesofContent {
+	
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lineNumber = 1;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+
+        scanner.close();
+	}
+
+}
+
+
+///Input Format
+
+//Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.

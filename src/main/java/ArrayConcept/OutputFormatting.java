d9e706c2c852d2afa85c5463db8ea0836d7112cd
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-20 14:53:17 +0530

    OutputFormatting

---
+++ src/main/java/ArrayConcept/OutputFormatting.java
@@ -0,0 +1,36 @@
+package ArrayConcept;
+
+import java.util.Scanner;
+
+public class OutputFormatting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("================================");
+		for (int i = 0; i < 3; i++) {
+			String str = scanner.next();
+			int num = scanner.nextInt();
+
+			// Print the string left-aligned with 15 characters and the integer with 3
+			// digits
+			System.out.printf("%-15s%03d%n", str, num);
+		}
+		System.out.println("================================");
+
+	}
+
+}
+
+//================================
+//Sample Input
+//java 100
+//cpp 65
+//python 50
+//
+//================================
+//Sample Output
+//java           100 
+//cpp            065 
+//python         050 

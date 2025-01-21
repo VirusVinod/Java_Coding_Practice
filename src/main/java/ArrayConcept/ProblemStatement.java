df5fb7905c464678aa713191325e035e1b6a797f
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-21 10:41:33 +0530

    ProblemStatement

---
+++ src/main/java/ArrayConcept/ProblemStatement.java
@@ -0,0 +1,12 @@
+package ArrayConcept;
+
+public class ProblemStatement {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for (System.out.print("Hi" + " "); i < 1; i++) {
+			System.out.println("Coding Ninjas");
+		}
+	}
+
+}

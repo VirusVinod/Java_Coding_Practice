805c7cb2181d6f30cb289fc0de763f9d26bbfe33
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-21 10:43:12 +0530

    Size of ArrayList

---
+++ src/main/java/ArrayConcept/ProblemStatement2.java
@@ -0,0 +1,15 @@
+package ArrayConcept;
+
+import java.util.ArrayList;
+
+public class ProblemStatement2 {
+
+	public static void main(String[] args) {
+		 ArrayList<String> list = new ArrayList<>();
+	        list.add("A");
+	        list.add("B");
+	        System.out.println("Size of ArrayList: " + list.size());
+
+	}
+
+}

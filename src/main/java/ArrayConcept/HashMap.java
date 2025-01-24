0599a08cc2f15840c9f113139de65371187a27ab
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-24 17:32:02 +0530

    Iterator

---
+++ src/main/java/ArrayConcept/HashMap.java
@@ -0,0 +1,20 @@
+package ArrayConcept;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class HashMap {
+
+	public static void main(String[] args) {
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
+		cars.add("Mazda");
+
+		Iterator<String> it = cars.iterator();
+
+		System.out.println(it.next());
+	}
+
+}

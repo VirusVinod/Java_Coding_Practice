9d7f8287b07b8aba1d0198b373bc6d987df3bcb7
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-24 17:33:50 +0530

    Iterator

--- src/main/java/ArrayConcept/HashMap.java
+++ src/main/java/ArrayConcept/HashMap.java
@@ -5,16 +5,35 @@ import java.util.Iterator;
 
 public class HashMap {
 
+//	example -1 
+//	public static void main(String[] args) {
+//		ArrayList<String> cars = new ArrayList<String>();
+//		cars.add("Volvo");
+//		cars.add("BMW");
+//		cars.add("Ford");
+//		cars.add("Mazda");
+//
+//		Iterator<String> it = cars.iterator();
+//
+//		System.out.println(it.next());
+//	}
+	
+//	example 2
+	
 	public static void main(String[] args) {
-		ArrayList<String> cars = new ArrayList<String>();
-		cars.add("Volvo");
-		cars.add("BMW");
-		cars.add("Ford");
-		cars.add("Mazda");
-
-		Iterator<String> it = cars.iterator();
-
-		System.out.println(it.next());
-	}
+	    ArrayList<Integer> numbers = new ArrayList<Integer>();
+	    numbers.add(12);
+	    numbers.add(8);
+	    numbers.add(2);
+	    numbers.add(23);
+	    Iterator<Integer> it = numbers.iterator();
+	    while(it.hasNext()) {
+	      Integer i = it.next();
+	      if(i < 10) {
+	        it.remove();
+	      }
+	    }
+	    System.out.println(numbers);
+	  }
 
 }

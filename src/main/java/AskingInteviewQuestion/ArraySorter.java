52219e2f7d69f955749094a08edb13b07e216a90
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-29 16:03:39 +0530

    Array Sorter

---
+++ src/main/java/AskingInteviewQuestion/ArraySorter.java
@@ -0,0 +1,30 @@
+package AskingInteviewQuestion;
+
+import java.util.Arrays;
+
+public class ArraySorter {
+	public static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+				
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 5, 2, 9, 1, 5, 6 };
+
+		Arrays.sort(array);
+		System.out.println("Sorted using Arrays.sort(): " + Arrays.toString(array));
+
+		int[] array2 = { 5, 2, 9, 1, 5, 6 };
+		bubbleSort(array2);
+		System.out.println("Sorted using Bubble Sort: " + Arrays.toString(array2));
+	}
+}

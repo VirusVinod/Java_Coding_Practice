1e032583e0b98d950a4765002098b0d6e5876c7f
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-30 14:56:58 +0530

    Min Max Array

---
+++ src/main/java/AskingInteviewQuestion/MinMaxArray.java
@@ -0,0 +1,22 @@
+package AskingInteviewQuestion;
+
+public class MinMaxArray {
+	public static void main(String[] args) {
+		int[] numbers = { 10, 20, 5, 6, 1, 8, 99, 45 };
+
+		int min = numbers[0];
+		int max = numbers[0];
+
+		for (int num : numbers) {
+			if (num < min) {
+				min = num;
+			}
+			if (num > max) {
+				max = num;
+			}
+		}
+
+		System.out.println("Smallest number: " + min);
+		System.out.println("Largest number: " + max);
+	}
+}

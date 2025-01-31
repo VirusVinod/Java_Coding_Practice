ee2810a40c54d78237f5e88382b6988e8a7e2ca2
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-01-31 10:42:38 +0530

    Reverse String

---
+++ src/main/java/AskingInteviewQuestion/ReverseString.java
@@ -0,0 +1,25 @@
+package AskingInteviewQuestion;
+
+public class ReverseString {
+	public static String reverse(String str) {
+		char[] charArray = str.toCharArray();
+		int left = 0, right = str.length() - 1;
+
+		while (left < right) {
+			char temp = charArray[left];
+			charArray[left] = charArray[right];
+			charArray[right] = temp;
+
+			left++;
+			right--;
+		}
+
+		return new String(charArray);
+	}
+
+	public static void main(String[] args) {
+		String input = "hello";
+		System.out.println("Original: " + input);
+		System.out.println("Reversed: " + reverse(input));
+	}
+}

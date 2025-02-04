fdbe0e5b6a10e6fc23c6ca401c7abe681892751e
VinodSingh680 <vinodkumarsingh680@gmail.com>
2025-02-04 19:32:02 +0530

    fb login

---
+++ src/test/java/Selenium/Test.java
@@ -0,0 +1,30 @@
+package Selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		
+		WebElement name = driver.findElement(By.id("email"));
+		name.sendKeys("vinos");
+		
+		WebElement pass = driver.findElement(By.id("pass"));
+		pass.sendKeys("vinos");
+		
+		WebElement log = driver.findElement(By.xpath("//button[@name='login']"));
+		log.click();
+		
+//		
+//		String acctualvalue = "aaaa";
+//		String expectedValue = "aaaa";
+//		Assert.assertEquals(expectedValue, acctualvalue);
+
+	}
+}

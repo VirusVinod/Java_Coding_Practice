aae3370260f89fd9aa658ff85f56711ee44e93ac
vinod singh <vinodkumarsingh680@gmail.com>
2025-02-12 22:01:53 +0530

    AnagramCheck

---
+++ src/main/java/InterviewQes/AnagramCheck.java
@@ -0,0 +1,30 @@
+package InterviewQes;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+	 public static boolean areAnagrams(String str1, String str2) {
+	        if (str1.length() != str2.length()) {
+	            return false;
+	        }
+
+	        char[] charArray1 = str1.toCharArray();
+	        char[] charArray2 = str2.toCharArray();
+
+	        Arrays.sort(charArray1);
+	        Arrays.sort(charArray2);
+	        return Arrays.equals(charArray1, charArray2);
+	    }
+
+	    public static void main(String[] args) {
+	        String str1 = "tomato";
+	        String str2 = "matoto";
+
+	        if (areAnagrams(str1, str2)) {
+	            System.out.println(str1 + " and " + str2 + " are anagrams.");
+	        } else {
+	            System.out.println(str1 + " and " + str2 + " are NOT anagrams.");
+	        }
+	    }
+}

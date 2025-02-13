055becb1b709acf9df0764ccf118e404d704cae6
vinod singh <vinodkumarsingh680@gmail.com>
2025-02-13 13:08:30 +0530

    Duplicate characte count

---
+++ src/main/java/InterviewQes/DuplicateCharacters.java
@@ -0,0 +1,38 @@
+package InterviewQes;
+
+
+public class DuplicateCharacters {
+	public static void main(String[] args) {
+        String str = "programming"; 
+        findDuplicateCharacters(str);
+    }
+
+    public static void findDuplicateCharacters(String str) {
+        char[] charArray = str.toCharArray();
+        int length = charArray.length;
+
+        System.out.println("Duplicate characters in the string:");
+
+        boolean hasDuplicates = false;
+        for (int i = 0; i < length; i++) {
+            if (charArray[i] == ' ') 
+                continue;
+
+            int count = 1;
+            for (int j = i + 1; j < length; j++) {
+                if (charArray[i] == charArray[j] && charArray[j] != '0') {
+                    count++;
+                    charArray[j] = '0'; 
+                }
+            }
+            if (count > 1) {
+                System.out.println(charArray[i] + ": " + count);
+                hasDuplicates = true;
+            }
+        }
+
+        if (!hasDuplicates) {
+            System.out.println("No duplicate characters found.");
+        }
+    }
+}

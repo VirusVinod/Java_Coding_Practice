677d96af34beef4acdbe616494def9436f639501
vinod singh <vinodkumarsingh680@gmail.com>
2025-02-14 21:15:43 +0530

    Selenium Dropdown list

---
+++ src/test/java/Selenium/dropdownList.java
@@ -0,0 +1,33 @@
+package Selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class dropdownList {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://seleniumpractise.blogspot.com/2016/08/bootstrap-dropdown-example-for-selenium.html");
+		
+		
+		WebElement Dropdown = driver.findElement(By.xpath("//button[@id='menu1']"));
+		Dropdown.click();
+		
+		List<WebElement> dropdownlist = driver.findElements(By.xpath("//ul[@class='dropdown-menu']//li//a"));
+		 for(WebElement we : dropdownlist) {
+			 String value = we.getText();	
+			 System.out.println(value);
+			 if(value.equals("JavaScript")) {
+				 we.click();
+				 break;
+			 }
+		 }
+
+	}
+
+}

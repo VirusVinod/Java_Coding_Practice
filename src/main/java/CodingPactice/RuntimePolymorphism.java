959d17076c9c1527bc8e5fa94bdd03ff94f23543
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-08 12:12:11 +0530

    code practice

---
+++ src/main/java/CodingPactice/RuntimePolymorphism.java
@@ -0,0 +1,22 @@
+package CodingPactice;
+
+class Bike {
+	void run() {
+		System.out.println("Running");
+	}
+}
+
+class Splander extends Bike {
+	void run() {
+		System.out.println("x Running safely with 60km");
+	}
+}
+
+public class RuntimePolymorphism {
+
+	public static void main(String[] args) {
+		Bike b = new Splander();
+		b.run();
+	}
+
+}

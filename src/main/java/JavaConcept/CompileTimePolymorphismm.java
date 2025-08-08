6e9640c703f5b994fd353e425e0a2c378eab632f
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-08 12:29:52 +0530

    code practice

---
+++ src/main/java/JavaConcept/CompileTimePolymorphismm.java
@@ -0,0 +1,23 @@
+package JavaConcept;
+
+public class CompileTimePolymorphismm {
+
+	int add(int a, int b) {
+
+		return a + b;
+
+	}
+
+	int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static void main(String[] args) {
+		
+		CompileTimePolymorphismm ctp = new CompileTimePolymorphismm();
+		System.out.println("Two no add : " + ctp.add(4, 100));
+		System.out.println("Three no add : " + ctp.add(4, 100, 3445));
+
+	}
+
+}

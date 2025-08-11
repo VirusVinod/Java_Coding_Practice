2f97dcb3d99c26b12314e72697f5508a9a96109a
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-11 08:52:30 +0530

    code practice

---
+++ src/main/java/CodingPactice/OccurrencesStr.java
@@ -0,0 +1,22 @@
+package CodingPactice;
+
+public class OccurrencesStr {
+
+	public static void main(String[] args) {
+
+		String org = "Automationin";
+		int[] n = new int[256];
+
+		for (int i = 0; i < org.length(); i++) {
+			n[org.charAt(i)]++;
+		}
+		
+		for(int j=0; j<256;j++) {
+			if(n[j]>1) {
+				System.out.println((char)j + ":"+n[j]);
+			}
+		}
+
+	}
+
+}

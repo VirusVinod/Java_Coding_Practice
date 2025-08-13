a544279f36cc7eaa6cf6bbb7ba1ebcef4987097c
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-13 10:36:07 +0530

    code pratice

---
+++ src/main/java/CodingPactice/fibonacci1.java
@@ -0,0 +1,19 @@
+package CodingPactice;
+
+public class fibonacci1 {
+
+	public static void main(String[] args) {
+		int n = 10, firstItem = 0, SecondItem = 1;
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print(firstItem + ",");
+
+			int nextItem = firstItem + SecondItem;
+			firstItem = SecondItem;
+			SecondItem = nextItem;
+
+		}
+
+	}
+
+}

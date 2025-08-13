a9c4609f46b031f9e9b3f8633c3c351dee291f7b
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-13 10:23:48 +0530

    code pratice

---
+++ src/main/java/JavaConceptPactice/CompileTimePolymorphismm3.java
@@ -0,0 +1,20 @@
+package JavaConceptPactice;
+
+public class CompileTimePolymorphismm3 {
+
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static void main(String[] args) {
+		
+		CompileTimePolymorphismm3 cc = new CompileTimePolymorphismm3();
+		System.out.println("Fisrts method :" + cc.add(12, 17) );
+		System.out.println("Second method :" + cc.add(12, 17,34) );
+	}
+
+}

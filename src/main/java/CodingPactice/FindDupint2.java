df2fa5dc6a786a6c9cf82921e6ca339d25c3d64e
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-13 10:32:23 +0530

    code pratice

---
+++ src/main/java/CodingPactice/FindDupint2.java
@@ -0,0 +1,27 @@
+package CodingPactice;
+
+public class FindDupint2 {
+
+	public static void main(String[] args) {
+
+		int[] abc = { 2, 1, 2, 1, 3, 4, 5, 7 };
+		int n = abc.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (abc[i] == abc[j]) {
+					isdup = true;
+					break;
+				}
+			}
+			
+			if(!isdup) {
+				System.out.print(abc[i]+",");
+			}
+
+		}
+
+	}
+
+}

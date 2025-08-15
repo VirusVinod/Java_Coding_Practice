759dbfff64d68a54c9a19bd89c75778d35629e40
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-15 10:27:40 +0530

    code practice

---
+++ src/main/java/JavaConceptPactice/RuntimePolymorphismm3.java
@@ -0,0 +1,29 @@
+package JavaConceptPactice;
+
+
+class Bikespl{
+	void run() {
+		
+		System.out.println("Bike is running");
+	}
+}
+
+
+class HeroBike extends Bikespl{
+	void run() {
+		System.out.println("Hero bike is running as per 60km");
+	}
+	
+}
+
+public class RuntimePolymorphismm3 {
+
+	public static void main(String[] args) {
+		
+		HeroBike bb = new HeroBike();
+		bb.run();
+	
+
+	}
+
+}

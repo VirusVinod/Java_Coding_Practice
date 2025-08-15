25b69c27948b71a24632957a0ba848e9a82e7d21
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-15 12:07:35 +0530

    code practice

---
+++ src/main/java/JavaConceptPactice/RuntimePolymorphismm1.java
@@ -0,0 +1,27 @@
+package JavaConceptPactice;
+
+
+class QA {
+	void testing() {
+		System.out.println("After Build QA perfrom the smoke testing");
+	}
+}
+
+
+class QAEngineer extends QA{
+	void testing() {
+		System.out.println("after build qa perform regration testing");
+	}
+}
+
+public class RuntimePolymorphismm1 {
+
+	public static void main(String[] args) {
+		
+		QA qa = new QAEngineer();
+		qa.testing();
+		
+
+	}
+
+}

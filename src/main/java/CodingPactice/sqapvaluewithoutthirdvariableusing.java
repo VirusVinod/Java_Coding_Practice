fccfbe71a34fe0cea920afff5fd3a442fae6d325
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-16 10:31:15 +0530

    code practice

---
+++ src/main/java/CodingPactice/sqapvaluewithoutthirdvariableusing.java
@@ -0,0 +1,24 @@
+package CodingPactice;
+
+public class sqapvaluewithoutthirdvariableusing {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b= 20;
+		
+		System.out.println("before a ____: " +a);
+		System.out.println("before b ____: " +b);
+				
+		
+		a =a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("after a : " +a);
+		System.out.println("after b : " +b);
+				
+
+	}
+
+}

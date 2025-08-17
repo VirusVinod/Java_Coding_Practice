33a458349b0feb76c641a0934907114c58838769
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-17 10:19:53 +0530

    code practice

---
+++ src/main/java/JavaConceptPactice/RuntimePolymorphismm5.java
@@ -0,0 +1,24 @@
+package JavaConceptPactice;
+
+class lap {
+	void laptop() {
+		System.out.println("This is a 7th gen leaptop");
+	}
+}
+
+class lappy extends lap {
+	void laptop() {
+		System.out.println("This is a i7 and 11th gen laptop");
+	}
+}
+
+public class RuntimePolymorphismm5 {
+
+	public static void main(String[] args) {
+		
+		lap ll = new lappy();
+		ll.laptop();
+
+	}
+
+}

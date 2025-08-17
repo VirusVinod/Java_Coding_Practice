16d6b30e1f7eced8ed03fed8128431c84a4677ac
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-17 10:16:58 +0530

    code practice

---
+++ src/main/java/JavaConceptPactice/CompileTimePolymorphismm5.java
@@ -0,0 +1,22 @@
+package JavaConceptPactice;
+
+public class CompileTimePolymorphismm5 {
+
+	int sum(int a, int b) {
+		return a + b;
+	}
+
+	int sum(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static void main(String[] args) {
+		
+		CompileTimePolymorphismm5 cpp = new CompileTimePolymorphismm5();
+	
+		System.out.println("call fisrt method : "+ cpp.sum(12, 120) );
+		System.out.println("call second method : "+ cpp.sum(12, 120, 456) );
+
+	}
+
+}

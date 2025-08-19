1fbb413e6ca3c174127cb9efc75cbb1aba6a90ad
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-19 09:18:13 +0530

    code practice

---
+++ src/main/java/CodingPactice/finddupstr3.java
@@ -0,0 +1,21 @@
+package CodingPactice;
+
+public class finddupstr3 {
+
+	public static void main(String[] args) {
+
+		String str = "Programmingn";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.print(res);
+
+	}
+
+}

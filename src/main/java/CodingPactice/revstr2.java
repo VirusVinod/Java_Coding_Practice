39c21ccd3919af6829b1d90b58900f55d173fc0e
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-19 10:43:45 +0530

    code practice

---
+++ src/main/java/CodingPactice/revstr2.java
@@ -0,0 +1,15 @@
+package CodingPactice;
+
+public class revstr2 {
+
+	public static void main(String[] args) {
+		String str = "laptop";
+		String res = "";
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + " " + res;
+		}
+		System.out.print(res);
+
+	}
+
+}

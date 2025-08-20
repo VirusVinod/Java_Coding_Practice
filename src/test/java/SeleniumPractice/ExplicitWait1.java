41cba3929e371db6dc61f4866fca68a2ceaa018f
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-20 09:38:22 +0530

    code practice

--- src/test/java/SeleniumPractice/ExplicitWait1.java
+++ src/test/java/SeleniumPractice/ExplicitWait1.java
@@ -10,23 +10,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ExplicitWait {
+public class ExplicitWait1 {
 
 	public static void main(String[] args) {
-		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		
 		
-		WebElement Careersclick = driver.findElement(By.xpath("//a[text()='Careers']"));
+		WebElement clickoncarrier = driver.findElement(By.xpath("//a[text()='Careers']"));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].scrollIntoView(true)", Careersclick);
-		wait.until(ExpectedConditions.visibilityOf(Careersclick));
-		js.executeScript("arguments[0].click()", Careersclick);
-//		Careersclick.click();
+		js.executeScript("arguments[0].scrollIntoView(true)", clickoncarrier);
+		wait.until(ExpectedConditions.visibilityOf(clickoncarrier));
+		js.executeScript("arguments[0].click()", clickoncarrier);
+
 	}
 
 }

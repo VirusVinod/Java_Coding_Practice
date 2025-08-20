e1bd38225b32037619d61d715616daa120887c4a
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-20 10:25:43 +0530

    code practice

---
+++ src/main/java/JavaConceptPactice/RuntimePolymorphismm7.java
@@ -0,0 +1,24 @@
+package JavaConceptPactice;
+
+class phone {
+	void iphone() {
+		System.out.println("Iphone 16");
+	}
+}
+
+class iphonemob extends phone {
+	void iphone() {
+		System.out.println("iphone is new launch 17");
+	}
+}
+
+public class RuntimePolymorphismm7 {
+
+	public static void main(String[] args) {
+
+		phone pp = new iphonemob();
+		pp.iphone();
+
+	}
+
+}

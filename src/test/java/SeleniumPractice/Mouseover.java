6b81d6017ebf7c7f4136449afd0341a9bb6f4b88
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-21 10:03:49 +0530

    code practice

---
+++ src/test/java/SeleniumPractice/Mouseover.java
@@ -0,0 +1,26 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Mouseover {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		WebElement hover = driver.findElement(By.xpath("//a//span[text()='Fresh']"));
+		Actions action = new Actions(driver);
+		action.moveToElement(hover).build().perform();
+
+	}
+
+}

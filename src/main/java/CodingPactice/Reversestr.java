3f0ce205d3b78741e427b73e74c30106092a01fa
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-21 11:08:43 +0530

    code practice

---
+++ src/main/java/CodingPactice/Reversestr.java
@@ -0,0 +1,15 @@
+package CodingPactice;
+
+public class Reversestr {
+
+	public static void main(String[] args) {
+		
+		String str = "vinod";
+		String res ="";
+		for(int i=0;i<str.length();i++) {
+			res= str.charAt(i)+" "+res;
+		}
+		System.out.print(res);
+	}
+
+}

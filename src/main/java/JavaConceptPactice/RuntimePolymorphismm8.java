26f71c3d482c8faffeac16621f6ff3d23cb68e28
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-24 06:12:48 +0530

    code practice

---
+++ src/main/java/JavaConceptPactice/RuntimePolymorphismm8.java
@@ -0,0 +1,23 @@
+package JavaConceptPactice;
+
+class one {
+	void run() {
+		System.out.println("calling one");
+	}
+}
+
+class two extends one {
+	void run() {
+		System.out.println("class to is callling");
+	}
+}
+
+public class RuntimePolymorphismm8 {
+
+	public static void main(String[] args) {
+		one oo = new two();
+		oo.run();
+
+	}
+
+}

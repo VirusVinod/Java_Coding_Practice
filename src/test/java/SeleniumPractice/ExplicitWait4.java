8e34a80f8eff7b38c16266f7de9c337f947d5aa4
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-24 06:53:05 +0530

    code practice

---
+++ src/test/java/SeleniumPractice/ExplicitWait4.java
@@ -0,0 +1,30 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait4 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+		WebElement ele = driver.findElement(By.xpath("//a[text()='Your Account']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", ele);
+		wait.until(ExpectedConditions.visibilityOf(ele));
+		js.executeScript("arguments[0].click()", ele);
+
+	}
+
+}

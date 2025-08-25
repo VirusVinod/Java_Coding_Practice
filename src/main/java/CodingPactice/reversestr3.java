d64ce2994045375070e49327f78bf824fd5f08cc
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-25 08:02:59 +0530

    code practice

---
+++ src/main/java/CodingPactice/reversestr3.java
@@ -0,0 +1,17 @@
+package CodingPactice;
+
+public class reversestr3 {
+
+	public static void main(String[] args) {
+
+		String str = "story";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + " " + res;
+		}
+		System.out.println(res);
+
+	}
+
+}

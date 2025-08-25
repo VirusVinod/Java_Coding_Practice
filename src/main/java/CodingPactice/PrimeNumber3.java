68b0c40e23cc25896ea549bd7890491b5469d655
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-25 22:03:54 +0530

    code practice

---
+++ src/main/java/CodingPactice/PrimeNumber3.java
@@ -0,0 +1,28 @@
+package CodingPactice;
+
+public class PrimeNumber3 {
+
+	public static void main(String[] args) {
+		int num = 15;
+		boolean isPrime = true;
+
+		if (num <= 1) {
+			isPrime = false;
+		} else {
+			for (int i = 2; i <= num / 2; i++) {
+				if (num % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+
+		}
+
+		if (isPrime)
+			System.out.println(num + " : is a prime number");
+		else
+			System.out.println(num + " : is a not prime number");
+
+	}
+
+}

3682989630b912fe299b864043a20379da9253f5
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-26 13:49:42 +0530

    code practice

---
+++ src/test/java/SeleniumPractice/screnshot5.java
@@ -0,0 +1,34 @@
+package SeleniumPractice;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class screnshot5 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		File screnshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File des = new File("sdskjfhskd.png");
+		try {
+			FileUtils.copyFile(screnshot, des);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+
+	}
+
+}

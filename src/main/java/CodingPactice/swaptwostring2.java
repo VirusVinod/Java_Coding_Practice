6828eee0b509c633cc529e6ef62a7331b644bbae
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-30 06:25:07 +0530

    Code Pratice

---
+++ src/main/java/CodingPactice/swaptwostring2.java
@@ -0,0 +1,18 @@
+package CodingPactice;
+
+public class swaptwostring2 {
+
+	public static void main(String[] args) {
+
+		String a = "Hi";
+		String b = "Stanger";
+
+		a = a + b;
+		b = a.substring(0, a.length() - b.length());
+		a = a.substring(b.length());
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+	}
+
+}

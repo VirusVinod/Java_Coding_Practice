3d8e145955b78289e427dc4d153f44979b5dc818
vinod singh <vinodkumarsingh680@gmail.com>
2025-08-31 10:24:35 +0530

    Code Pratice

---
+++ src/test/java/SeleniumPractice/screnshot9.java
@@ -0,0 +1,30 @@
+package SeleniumPractice;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class screnshot9 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://github.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		File screnshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File des = new File("file.png");
+		try {
+			FileUtils.copyFile(screnshot, des);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

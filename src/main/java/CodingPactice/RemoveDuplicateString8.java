5f8ca707ac580db70ac1814bdda045d53e0d0088
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-03 09:59:09 +0530

    Code Pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateString8.java
@@ -0,0 +1,19 @@
+package CodingPactice;
+
+public class RemoveDuplicateString8 {
+
+	public static void main(String[] args) {
+		String str = "AutomationAutomation";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+
+		}
+		System.out.print(res);
+	}
+
+}

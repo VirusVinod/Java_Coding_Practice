b43bfbe5e2357d5bd28bb68837c9c0f8815e3ecd
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-03 09:00:19 +0530

    Code Pratice

---
+++ src/test/java/SeleniumPractice/ExplicitWait8.java
@@ -0,0 +1,32 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait8 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		
+		WebElement eleclick = driver.findElement(By.xpath("//a[text()='Amazon Global Selling']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", eleclick);
+		wait.until(ExpectedConditions.visibilityOf(eleclick));
+		js.executeScript("arguments[0].click()", eleclick);
+		
+		driver.quit();
+
+	}
+
+}

44aede781f8309536c08a2a6c479224cbf628732
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-06 10:01:30 +0530

    code prtice

---
+++ src/main/java/CodingPactice/RightAlignedPyramid1.java
@@ -0,0 +1,20 @@
+package CodingPactice;
+
+public class RightAlignedPyramid1 {
+
+	public static void main(String[] args) {
+		int n = 5;
+
+		for (int i = 0; i <= n; i++) {
+			for (int j = i; j <= n; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}

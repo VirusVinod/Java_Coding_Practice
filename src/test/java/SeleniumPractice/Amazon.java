433f2ec82ad1e0e466b6dec7825d4fc1c85d0ab4
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-07 13:55:18 +0530

    code prtice

--- src/test/java/SeleniumPractice/Amazon.java
+++ src/test/java/SeleniumPractice/Amazon.java
@@ -1,8 +1,10 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,13 +16,28 @@ public class Amazon {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
-		
+
 		WebElement searchProduct = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchProduct.sendKeys("Iphone");
-		
+
 		WebElement afterSearchClickBtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		afterSearchClickBtn.click();
+
+		String tragetProductName = "Apple iPhone 15 (128 GB) - Black";
+		List<WebElement> getAllProductList = driver
+				.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+		
+		for (WebElement product : getAllProductList) {
+			String getProductName = product.getText();
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollIntoView(true)", product);
+			if (getProductName.equalsIgnoreCase(tragetProductName)) {
+			
+				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+		}
 
 	}
 

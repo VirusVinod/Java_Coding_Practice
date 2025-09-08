03dd48d4bc1ebb6ac08a3baebfcd24d6eb0ca9f2
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-08 06:58:18 +0530

    code prtice Selenium

---
+++ src/test/java/SeleniumPractice/LoginPage1.java
@@ -0,0 +1,29 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage1 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		WebElement userName = driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
+
+		WebElement pass = driver.findElement(By.id("password"));
+		pass.sendKeys("secret_sauce");
+
+		WebElement btnClick = driver.findElement(By.id("login-button"));
+		btnClick.click();
+
+	}
+
+}

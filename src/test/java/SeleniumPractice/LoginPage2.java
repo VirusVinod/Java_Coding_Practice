5ac8da17f721106f65188ffd7a0c11581790af17
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-09 11:14:29 +0530

    code prtice Selenium

--- src/test/java/SeleniumPractice/LoginPage2.java
+++ src/test/java/SeleniumPractice/LoginPage2.java
@@ -1,8 +1,13 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,14 +32,23 @@ public class LoginPage2 {
 
 		WebElement lognbtn = driver.findElement(By.id("login-button"));
 		lognbtn.click();
-		
+
 		// Step: 2
-		
+
 		String validatedNavigateUrl = "Products";
 		WebElement pageNameGet = driver.findElement(By.xpath("//div[@class='product_label']"));
 		String acutualRes = pageNameGet.getText();
 		Assert.assertEquals(validatedNavigateUrl, acutualRes);
-				
+
+		// Step: 3
+
+		File screnshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File dest = new File("Login.png");
+		try {
+			FileUtils.copyFile(screnshot, dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 

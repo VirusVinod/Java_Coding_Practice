59aaa6f4db626bbdbda64aa996269e87c6f2b570
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-09 11:30:21 +0530

    code prtice Selenium

--- src/test/java/SeleniumPractice/LoginPage2.java
+++ src/test/java/SeleniumPractice/LoginPage2.java
@@ -3,9 +3,11 @@ package SeleniumPractice;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -49,6 +51,25 @@ public class LoginPage2 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		// Step: 4
+
+		String ExpectedProduct = "Sauce Labs Backpack";
+		List<WebElement> getProdcatName = driver.findElements(By.xpath("//div[@class='inventory_item_name']"));
+
+		for (WebElement allProductget : getProdcatName) {
+			String actualProduct = allProductget.getText();
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollIntoView(true)", allProductget);
+			if (actualProduct.equals(ExpectedProduct)) {
+				WebElement clickAddToCartItem = driver
+						.findElement(By.xpath("//button[@class='btn_primary btn_inventory']"));
+				js.executeScript("arguments[0].click()", clickAddToCartItem);
+			}
+
+		}
+		
+		driver.quit();
 
 	}
 

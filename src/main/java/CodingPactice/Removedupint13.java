1c583c6655eb714fee28726e138a209b50da8207
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-10 19:27:45 +0530

    code prtice

---
+++ src/main/java/CodingPactice/Removedupint13.java
@@ -0,0 +1,20 @@
+package CodingPactice;
+
+public class Removedupint13 {
+
+	public static void main(String[] args) {
+
+		String str = "HiiHiiHii";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.print(res);
+
+	}
+
+}

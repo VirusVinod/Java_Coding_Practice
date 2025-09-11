54a242e5711a540e7d65d85c2d1df0679d791733
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-11 20:14:03 +0530

    code pratice

---
+++ src/main/java/CodingPactice/Removedupint14.java
@@ -0,0 +1,25 @@
+package CodingPactice;
+
+public class Removedupint14 {
+
+	public static void main(String[] args) {
+		int[] value = { 11, 13, 14, 22, 11, 19, 12, 14 };
+		int n = value.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (value[i] == value[j]) {
+					isdup = true;
+					break;
+
+				}
+			}
+			if (!isdup) {
+				System.out.print(value[i] + ",");
+			}
+		}
+
+	}
+
+}

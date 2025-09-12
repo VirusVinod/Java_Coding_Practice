57b3f9e89652da32c8da4b2b6fdff860de4597d3
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-12 07:02:29 +0530

    code pratice

---
+++ src/main/java/CodingPactice/Removedupint15.java
@@ -0,0 +1,28 @@
+package CodingPactice;
+
+public class Removedupint15 {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 3, 2, 1 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+
+			for (int j = 0; j < i; j++) {
+
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+
+				}
+
+			}
+			if (!isdup) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+
+	}
+
+}

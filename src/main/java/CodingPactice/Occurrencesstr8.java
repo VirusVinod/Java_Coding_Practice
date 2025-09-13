bba929b408623d09693eb7c662ad7715a7ba0b3a
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-13 09:22:48 +0530

    code pratice

---
+++ src/main/java/CodingPactice/Occurrencesstr8.java
@@ -0,0 +1,20 @@
+package CodingPactice;
+
+public class Occurrencesstr8 {
+
+	public static void main(String[] args) {
+		String str = "HiHi";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			count[str.charAt(i)]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " : " + count[j]);
+			}
+		}
+	}
+
+}

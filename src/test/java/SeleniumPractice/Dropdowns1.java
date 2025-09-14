2dbff70d492feef75f8e13ac3df2e94614153ebd
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-14 12:03:04 +0530

    code pratice selenium

---
+++ src/test/java/SeleniumPractice/Dropdowns1.java
@@ -0,0 +1,26 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Dropdowns1 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.hyrtutorials.com/p/html-dropdown-elements-practice.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		WebElement SelectValueDropdown = driver.findElement(By.xpath("//select[@id=\"course\"]"));
+
+		Select s = new Select(SelectValueDropdown);
+		s.selectByVisibleText("Python");
+
+	}
+
+}

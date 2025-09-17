c5a71129910a7f5d51f3ca1f55abbe020040b204
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-17 10:06:50 +0530

    Selenium code pratice

--- src/test/java/SeleniumPractice/LoginPage3.java
+++ src/test/java/SeleniumPractice/LoginPage3.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Driver;
 import java.time.Duration;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -23,6 +25,7 @@ public class LoginPage3 {
 		ll.login();
 		ll.validationCheck();
 		ll.screnshot();
+		ll.addToCart();
 
 	}
 
@@ -74,7 +77,33 @@ public class LoginPage3 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+//	Step : 5
+
+	public void addToCart() {
+
+		String ExpectedProduct = "Sauce Labs Backpack";
+		List<WebElement> findAllProduct = driver.findElements(By.xpath("//div[@class='inventory_item_name']"));
+
+		for (WebElement getAllData : findAllProduct) {
+
+			String actulaProductName = getAllData.getText();
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollIntoView(true)", getAllData);
+
+			if (actulaProductName.equals(ExpectedProduct)) {
+
+				WebElement addToCartPro = driver.findElement(By.xpath("//button[@class='btn_primary btn_inventory']"));
+				js.executeScript("arguments[0].click()", addToCartPro);
+				break;
+
+			}
+
+		}
 		driver.quit();
+
 	}
 
 }

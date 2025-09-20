e20ab7707bf80a93bba77eb790ec45078d3e4802
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-20 12:45:38 +0530

    code pratice

---
+++ src/main/java/CodingPactice/ModulusExample.java
@@ -0,0 +1,28 @@
+package CodingPactice;
+
+import java.util.Scanner;
+
+public class ModulusExample {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value : ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int c = gcd(a, b); // modulus operation
+		System.out.println(c);
+
+		sc.close();
+	}
+
+	static int gcd(int x, int y) {
+		while (y != 0) {
+			int temp = y;
+			y = x % y;
+			x = temp;
+		}
+		return x;
+	}
+
+}

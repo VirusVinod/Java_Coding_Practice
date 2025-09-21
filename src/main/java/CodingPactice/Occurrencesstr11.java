7de0ac5512c87a2c5cc3ca06658e3c1bf3a44f84
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-21 05:30:31 +0530

    Code Pratice

---
+++ src/main/java/CodingPactice/Occurrencesstr11.java
@@ -0,0 +1,20 @@
+package CodingPactice;
+
+public class Occurrencesstr11 {
+
+	public static void main(String[] args) {
+		String str = "hihi";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			count[str.charAt(i)]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] >= 1) {
+				System.out.println((char) j + " : " + count[j]);
+			}
+		}
+	}
+
+}

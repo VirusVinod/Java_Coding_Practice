98e9b4f37c70b047e47d397f4d29febc91cff1ae
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-22 22:22:44 +0530

    Code Pratice

---
+++ src/main/java/leetcode/ReverseInteger.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+
+		int x = 123;
+		int res = 0;
+
+		while (x != 0) {
+			int digit = x % 10;
+			res = res * 10 + digit;
+			x /= 10;
+
+		}
+
+		System.out.println(res);
+
+	}
+
+}

2fb67578a16da3e34ef6d1c20c470e336bc7953b
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-24 14:37:41 +0530

    Code Pratice

---
+++ src/main/java/CodingPactice/FibonacciSeries18.java
@@ -0,0 +1,21 @@
+package CodingPactice;
+
+public class FibonacciSeries18 {
+
+	public static void main(String[] args) {
+
+		int n = 10, first = 0, second = 1;
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.print(first + ",");
+
+			int tempval = first + second;
+			first = second;
+			second = tempval;
+
+		}
+
+	}
+
+}

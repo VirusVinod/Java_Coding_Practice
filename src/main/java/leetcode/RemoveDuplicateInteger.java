2ecc9cd41fcd4caaa4afdee6c97b9dd0bd1b93a1
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-26 19:34:09 +0530

    Code Pratice

---
+++ src/main/java/leetcode/RemoveDuplicateInteger.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class RemoveDuplicateInteger {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 1, 2, 2, 1, 3, 4, 3, 4 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+				}
+			}
+			if (!isdup) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+
+	}
+
+}

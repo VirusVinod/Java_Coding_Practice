2a518edd065a2b536df73f2be41f97b13538fbf6
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-27 11:57:41 +0530

    Selenium Code Pratice

--- src/test/java/SeleniumPractice/popuphandle.java
+++ src/test/java/SeleniumPractice/popuphandle.java
@@ -1,5 +1,48 @@
 package SeleniumPractice;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class popuphandle {
+	public static void main(String[] args) {
+
+		ChromeOptions options = new ChromeOptions();
+
+		// Disable password manager popup
+		Map<String, Object> prefs = new HashMap<>();
+		prefs.put("credentials_enable_service", false);
+		prefs.put("profile.password_manager_enabled", false);
+		options.setExperimentalOption("prefs", prefs);
+
+		// Disable notifications
+		options.addArguments("--disable-notifications");
+
+		// Force a fresh Chrome profile (no sync, no saved passwords)
+		options.addArguments("--guest");
+		// or: options.addArguments("--incognito");
+
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		driver.get("https://www.saucedemo.com/v1/");
+
+		WebElement userName = driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
+
+		WebElement pass = driver.findElement(By.id("password"));
+		pass.sendKeys("secret_sauce");
+
+		WebElement btnClick = driver.findElement(By.id("login-button"));
+		btnClick.click();
+	}
 
 }

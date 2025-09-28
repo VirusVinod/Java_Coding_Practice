41d4c852c293d567c93613d1a9e3ad3ae9c12a14
vinod singh <vinodkumarsingh680@gmail.com>
2025-09-28 15:10:09 +0530

    Java Code Pratice

---
+++ src/main/java/leetcode/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+		int n = 10, first = 0, second = 1;
+
+		for (int i = 0; i <= n; i++) {
+			System.out.print(first + ",");
+
+			int nextItem = first + second;
+			first = second;
+			second = nextItem;
+		}
+
+	}
+
+}

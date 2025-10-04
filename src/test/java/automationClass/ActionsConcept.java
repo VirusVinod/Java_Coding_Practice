e4673c263add3eb3f27a5ac1696d70b2c9b88de4
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-04 11:16:06 +0530

    Selenium  Code Pratice

--- src/test/java/automationClass/ActionsConcept.java
+++ src/test/java/automationClass/ActionsConcept.java
@@ -1,31 +1,52 @@
 package automationClass;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionsConcept {
-	
+
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.browserstack.com/guide/mouse-hover-in-selenium");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-		
+
 		WebElement productBtn = driver.findElement(By.xpath("//button[@id='products-dd-toggle']"));
-		
+
 		Actions a = new Actions(driver);
 		a.moveToElement(productBtn).perform();
-		
+
 		WebElement InnerApptesting = driver.findElement(By.xpath("//button[@id='products-dd-tab-3']"));
-		Actions aa = new Actions(driver);
-		aa.moveToElement(InnerApptesting).perform();
+		a.moveToElement(InnerApptesting).perform();
+
+		
+		
+		String expectedRes = "App Live";
+		List<WebElement> allProd = driver.findElements(By.xpath("//div[@id='products-dd-tabpanel-3-inner-1']//a"));
+
+		for (WebElement ele : allProd) {
+		    String actual = ele.getText().trim();
+
+		    if (actual.contains(expectedRes)) {  // <- use contains to handle extra text
+		        JavascriptExecutor js = (JavascriptExecutor) driver;
+		        js.executeScript("arguments[0].scrollIntoView(true);", ele);
+		        js.executeScript("arguments[0].click();", ele);
+
+		        System.out.println("Target Product Name: " + expectedRes);
+		        System.out.println("Clicked Product Text: " + actual);
+		        break;
+		    }
+		}
+
 	}
 
 }

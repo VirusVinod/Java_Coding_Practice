3c51b588e24592fd518484033f73f088ac7ae27b
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-04 10:52:11 +0530

    Selenium  Code Pratice

---
+++ src/test/java/automationClass/ActionsConcept.java
@@ -0,0 +1,27 @@
+package automationClass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsConcept {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.browserstack.com/guide/mouse-hover-in-selenium");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		
+		WebElement productBtn = driver.findElement(By.xpath("//button[@id='products-dd-toggle']"));
+		
+		Actions a = new Actions(driver);
+		a.moveToElement(productBtn).perform();
+	}
+
+}

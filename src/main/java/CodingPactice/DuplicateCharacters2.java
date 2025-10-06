ae823ff5b41a3eafef66ca0c0326cb80e3985fab
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-06 13:15:14 +0530

    Java Code Pratice

---
+++ src/main/java/CodingPactice/DuplicateCharacters2.java
@@ -0,0 +1,24 @@
+package CodingPactice;
+
+public class DuplicateCharacters2 {
+
+	public static void main(String[] args) {
+
+		String str = "aaautomationann";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " - " + count[j]);
+			}
+
+		}
+
+	}
+
+}

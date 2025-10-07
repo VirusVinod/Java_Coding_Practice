fc99a4b3ee553f244eca56a281aeb1f23df737b7
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-07 11:19:35 +0530

    Selenium  Code Pratice

--- src/test/java/SeleniumPractice/Amazon3.java
+++ src/test/java/SeleniumPractice/Amazon3.java
@@ -2,20 +2,30 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon3 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step : 01
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
+//		Step : 02
+
+		WebElement Search = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		Search.sendKeys("Iphone");
+
+		WebElement Searchclick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		Searchclick.click();
+
 		
 	}
 

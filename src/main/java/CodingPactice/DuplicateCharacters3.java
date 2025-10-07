a25814c418fa0d90ca37a1624bdba193742b0473
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-07 10:53:08 +0530

    Java Code Pratice

---
+++ src/main/java/CodingPactice/DuplicateCharacters3.java
@@ -0,0 +1,22 @@
+package CodingPactice;
+
+public class DuplicateCharacters3 {
+
+	public static void main(String[] args) {
+
+		String str = "AABBCCCGAAGGTYUUUUU";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " - " + count[j]);
+			}
+		}
+	}
+
+}

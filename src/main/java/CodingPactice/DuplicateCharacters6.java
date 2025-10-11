14e730d169540956264500060364bbf6aaf6141f
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-11 10:46:06 +0530

    Java Code Pratice

---
+++ src/main/java/CodingPactice/DuplicateCharacters6.java
@@ -0,0 +1,22 @@
+package CodingPactice;
+
+public class DuplicateCharacters6 {
+
+	public static void main(String[] args) {
+
+		String str = "dsfsfdfdfdf";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " : " + count[j]);
+			}
+		}
+	}
+
+}

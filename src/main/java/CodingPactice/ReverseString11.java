7b83fe36253fb8c6ab5751cfd5b3c9599e23c7fe
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-13 15:55:14 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/ReverseString11.java
@@ -0,0 +1,16 @@
+package CodingPactice;
+
+public class ReverseString11 {
+
+	public static void main(String[] args) {
+
+		String str = "sdfhsk";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + res;
+		}
+		System.out.print(res.toUpperCase());
+	}
+
+}

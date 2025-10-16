dcad742b014d4380174b14da65b6cf40c0956f20
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-16 20:11:32 +0530

    Java Code Pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateInteger9.java
@@ -0,0 +1,22 @@
+package CodingPactice;
+
+public class RemoveDuplicateInteger9 {
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 10, 20 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+				}
+			}
+			if (!isdup) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

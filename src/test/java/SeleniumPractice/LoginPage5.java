c6611823bea1ac195ebe8e77d47da55187b7fef5
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-20 12:06:37 +0530

    Selenium Code Pratice

--- src/test/java/SeleniumPractice/LoginPage5.java
+++ src/test/java/SeleniumPractice/LoginPage5.java
@@ -2,7 +2,9 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage5 {
@@ -13,6 +15,20 @@ public class LoginPage5 {
 		driver.get("https://www.saucedemo.com/v1/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+//		Step : 02
+
+		WebElement userName = driver.findElement(By.id("user-name"));
+		userName.sendKeys("standard_user");
+
+		WebElement pass = driver.findElement(By.id("password"));
+		pass.sendKeys("secret_sauce");
+
+		WebElement btnClick = driver.findElement(By.id("login-button"));
+		btnClick.click();
+
+		
+
 
 	}
 

e1ed5530b250f49f6dd039aafcaa7539f524d465
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-21 18:04:26 +0530

    Java  Code Pratice

---
+++ src/main/java/CodingPactice/ReverseString16.java
@@ -0,0 +1,15 @@
+package CodingPactice;
+
+public class ReverseString16 {
+	public static void main(String[] args) {
+
+		String str = "CODE";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + res;
+		}
+		System.out.print(res);
+	}
+
+}

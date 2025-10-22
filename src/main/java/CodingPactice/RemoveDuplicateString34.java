2ed4f595da3a1f574d3c0814723cdc27a54a1a3c
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-22 09:53:16 +0530

    Java  Code Pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateString34.java
@@ -0,0 +1,18 @@
+package CodingPactice;
+
+public class RemoveDuplicateString34 {
+	public static void main(String[] args) {
+
+		String str = "removeremove";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.println(res);
+	}
+
+}

520db5fce8be86bff1cdfb46b5216d2593bfd183
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-23 18:29:07 +0530

    Java  Code Pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateInteger14.java
@@ -0,0 +1,20 @@
+package CodingPactice;
+
+public class RemoveDuplicateInteger14 {
+
+	public static void main(String[] args) {
+
+		String str = "codeselenium";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.print(res);
+
+	}
+
+}

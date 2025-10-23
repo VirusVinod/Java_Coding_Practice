5b4b125f1b48b9f89bda44dabf2250bd7b6f5ffc
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-23 18:31:32 +0530

    Java  Code Pratice

---
+++ src/main/java/CodingPactice/Swapstr9.java
@@ -0,0 +1,18 @@
+package CodingPactice;
+
+public class Swapstr9 {
+
+	public static void main(String[] args) {
+
+		String a = "123";
+		String b = "879";
+
+		a = a + b;
+		b = a.substring(0, a.length() - b.length());
+		a = a.substring(b.length());
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+	}
+
+}

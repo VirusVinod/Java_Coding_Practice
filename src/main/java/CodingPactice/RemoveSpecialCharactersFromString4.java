516cf9dd15bc4cf1a77cf64d7aa82855f0fd5bca
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-24 19:41:58 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/RemoveSpecialCharactersFromString4.java
@@ -0,0 +1,20 @@
+package CodingPactice;
+
+public class RemoveSpecialCharactersFromString4 {
+	public static void main(String[] args) {
+
+		String str = "lap@#$%^&*()(topyyyass";
+		String clean = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || ch == ' ') {
+				clean = clean + ch;
+			}
+
+		}
+		System.out.println(clean.toString());
+
+	}
+
+}

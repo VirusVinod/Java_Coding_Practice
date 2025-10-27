493e6adbd4e39d747b35e8a1321548aab12e97c9
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-27 16:48:53 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateInteger19.java
@@ -0,0 +1,23 @@
+package CodingPactice;
+
+public class RemoveDuplicateInteger19 {
+	public static void main(String[] args) {
+
+		int[] arr = { 2, 2, 2, 1, 1, 1, 233, 32 };
+		int n = arr.length;
+
+		for (int i = 0; i < arr.length; i++) {
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+				}
+			}
+			if (!isdup) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

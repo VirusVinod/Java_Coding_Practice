c8220d800bedafb5c1db502723175356b1b04d05
vinod singh <vinodkumarsingh680@gmail.com>
2025-10-28 18:54:52 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/FibonacciSeries37.java
@@ -0,0 +1,17 @@
+package CodingPactice;
+
+public class FibonacciSeries37 {
+	public static void main(String[] args) {
+		int n = 10, first = 0, second = 1;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(first + ",");
+
+			int item = first + second;
+			first = second;
+			second = item;
+		}
+
+	}
+
+}

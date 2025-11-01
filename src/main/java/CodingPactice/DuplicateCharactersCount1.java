33de46d1a5503aae7a722fa49c92e8961d6ffd36
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-01 19:22:14 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/DuplicateCharactersCount1.java
@@ -0,0 +1,22 @@
+package CodingPactice;
+
+public class DuplicateCharactersCount1 {
+
+	public static void main(String[] args) {
+		String str = "erefdfaaauuuttdsadttttttydsdsdsd";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " - " + count[j]);
+			}
+		}
+
+	}
+
+}

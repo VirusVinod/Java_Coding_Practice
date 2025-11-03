671070b6f7e62d95b16e2f5272c7549dd1d0e153
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-03 08:57:18 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateInteger22.java
@@ -0,0 +1,24 @@
+package CodingPactice;
+
+public class RemoveDuplicateInteger22 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 1, 2, 2, 3, 1, 4 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+				}
+			}
+			if (!isdup) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

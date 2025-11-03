4c129359017aa7436ca1c3c2fb476d0b71c9e9f8
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-03 09:10:13 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/SwapTwovariable1.java
@@ -0,0 +1,16 @@
+package CodingPactice;
+
+public class SwapTwovariable1 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 30;
+
+		a = a + b;
+		b = a - b;
+		a = a - b;
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+	}
+
+}

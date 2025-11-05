f02c3f36a6df487d8eeb1bff48648c3232bea3fd
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-05 19:33:09 +0530

    Java Code Pratice

---
+++ src/main/java/CodingPactice/RemoveDuplicateInteger32.java
@@ -0,0 +1,26 @@
+package CodingPactice;
+
+public class RemoveDuplicateInteger32 {
+	public static void main(String[] args) {
+
+		int[] arr = { 2, 2, 23, 3, 4, 5, 23 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+
+			boolean isdup = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+				}
+			}
+			if (!isdup) {
+				System.out.print(arr[i] + ",");
+			}
+
+		}
+
+	}
+
+}

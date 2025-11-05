a7db7e1e7166b5bfb2b2b00f0935efef5a5407df
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-05 19:22:05 +0530

    Java Code Pratice

---
+++ src/main/java/CodingPactice/areAnagrams3.java
@@ -0,0 +1,38 @@
+package CodingPactice;
+
+import java.util.Arrays;
+
+public class areAnagrams3 {
+
+	public static void main(String[] args) {
+
+		String str1 = "data";
+		String str2 = "aadt";
+
+		boolean isAnagram = true;
+
+		if (str1.length() != str2.length()) {
+			isAnagram = false;
+		} else {
+			char[] ch1 = str1.toCharArray();
+			char[] ch2 = str2.toCharArray();
+
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+
+			if (!Arrays.equals(ch1, ch2)) {
+
+				isAnagram = false;
+
+			}
+
+		}
+		if (isAnagram) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+
+	}
+
+}

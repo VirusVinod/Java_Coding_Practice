1f1694c80fef43713f6aae4fba122c4565eefb21
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-11 10:39:35 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/areAnagrams4.java
@@ -0,0 +1,35 @@
+package codingpactice1;
+
+import java.util.Arrays;
+
+public class areAnagrams4 {
+
+	public static void main(String[] args) {
+
+		String str1 = "data";
+		String str2 = "taad";
+
+		boolean isAnagram = true;
+
+		if (str1.length() != str2.length()) {
+			isAnagram = false;
+		} else {
+
+			char[] ch1 = str1.toCharArray();
+			char[] ch2 = str2.toCharArray();
+
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+
+			if (!Arrays.equals(ch1, ch2)) {
+				isAnagram = false;
+			}
+		}
+		if (isAnagram) {
+			System.out.println("True");
+		} else {
+			System.out.println("false");
+		}
+	}
+
+}

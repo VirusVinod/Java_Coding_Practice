ec66d466912205be78662bb5f997d4eeb0051440
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-13 10:15:20 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/demoqa.java
+++ src/test/java/SeleniumPractice/demoqa.java
@@ -1,12 +1,14 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class demoqa {
 
@@ -21,8 +23,30 @@ public class demoqa {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", maleRadio);
 
+//		DatePicker
 		WebElement maleRadatePickerInputdio = driver.findElement(By.id("dateOfBirthInput"));
 		js.executeScript("arguments[0].click()", maleRadatePickerInputdio);
+
+		WebElement monthDropDown = driver.findElement(By.xpath(" //select[@class='react-datepicker__month-select']"));
+		Select s = new Select(monthDropDown);
+		s.selectByVisibleText("October");
+
+		WebElement yearDropDown = driver.findElement(By.xpath(" //select[@class='react-datepicker__year-select']"));
+		Select y = new Select(yearDropDown);
+		y.selectByVisibleText("2023");
+
+		String ExpectdatedDate = "8";
+		List<WebElement> allDates = driver.findElements(By.xpath(" //div[@role='option']"));
+
+		for (WebElement a : allDates) {
+			String date = a.getText();
+			System.out.println(date);
+
+			if (date.equals(ExpectdatedDate)) {
+				a.click();
+				break;
+			}
+		}
 
 	}
 

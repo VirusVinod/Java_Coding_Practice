8e9574f7af9a8f2c153d011e83a12fcff8ae0abe
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-13 10:04:52 +0530

    selenium code pratice

---
+++ src/test/java/SeleniumPractice/demoqa.java
@@ -0,0 +1,27 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demoqa {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/automation-practice-form");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		WebElement maleRadio = driver.findElement(By.id("gender-radio-1"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", maleRadio);
+
+	
+	}
+
+}

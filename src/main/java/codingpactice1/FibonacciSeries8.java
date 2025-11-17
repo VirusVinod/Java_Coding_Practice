fd65dfb4b02f7fb3d43dfb6cbf68c22b66f4b85d
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-17 09:35:29 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/FibonacciSeries8.java
@@ -0,0 +1,18 @@
+package codingpactice1;
+
+public class FibonacciSeries8 {
+
+	public static void main(String[] args) {
+
+		int n = 10, first = 0, second = 1;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(first + ",");
+
+			int digit = first + second;
+			first = second;
+			second = digit;
+		}
+	}
+
+}

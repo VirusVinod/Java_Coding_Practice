b5362464ca15e0e7654424b5d6342f7cf8e80c93
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-17 09:41:52 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/RemoveDuplicateString7.java
@@ -0,0 +1,20 @@
+package codingpactice1;
+
+public class RemoveDuplicateString7 {
+
+	public static void main(String[] args) {
+
+		String str = "LKJHGHJKLKJHG";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.println(res);
+	}
+
+}

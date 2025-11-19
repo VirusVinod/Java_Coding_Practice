ab607d3e538803da7891d2a48f7df869f81a61b5
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-19 14:08:12 +0530

    Java Code Pratice

---
+++ src/main/java/codingpactice1/SwapTwoString5.java
@@ -0,0 +1,15 @@
+package codingpactice1;
+
+public class SwapTwoString5 {
+	public static void main(String[] args) {
+
+		String str = "DATA";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + res;
+		}
+		System.out.println(res);
+	}
+
+}

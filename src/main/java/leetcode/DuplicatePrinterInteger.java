1477754f395d2d4acc90a8f1fffa79f5a0401c5f
vinod singh <vinodkumarsingh680@gmail.com>
2025-11-20 13:24:51 +0530

    Java Code Pratice

--- src/main/java/leetcode/DuplicatePrinterInteger.java
+++ src/main/java/leetcode/DuplicatePrinterInteger.java
@@ -7,6 +7,29 @@ public class DuplicatePrinterInteger {
 		int[] arr = { 1, 2, 4, 2, 4, 2 };
 		int n = arr.length;
 
+		System.out.print("Duplicate integers: ");
+
+		for (int i = 0; i < n; i++) {
+			boolean isdup = false;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isdup = true;
+					break;
+				}
+			}
+
+			if (isdup) {
+				continue;
+			}
+
+			for (int k = i + 1; k < n; k++) {
+				if (arr[i] == arr[k]) {
+					System.out.print(arr[i] + " ");
+					break;
+				}
+			}
+		}
 	}
 
 }

db92fe13e48293f412593d75e4a1cd59f7493eff
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-01 09:49:23 +0530

    java code pratice

---
+++ src/main/java/CodingPactice/FindMaximumValueInArray29.java
@@ -0,0 +1,17 @@
+package CodingPactice;
+
+public class FindMaximumValueInArray29 {
+
+	public static void main(String[] args) {
+		int[] arr = { 23, 45, 66, 21, 54, 33 };
+		int max = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		System.out.println(max);
+	}
+
+}

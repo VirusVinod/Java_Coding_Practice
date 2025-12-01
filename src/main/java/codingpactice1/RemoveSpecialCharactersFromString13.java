e9b4017f57fb89b677da0ce12f05250d4c5eab05
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-01 09:35:59 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/RemoveSpecialCharactersFromString13.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class RemoveSpecialCharactersFromString13 {
+
+	public static void main(String[] args) {
+
+		String str = "$%^&*(VIN$%^&*OD";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || ch == ' ') {
+				res = res + ch;
+			}
+		}
+		System.out.println(res);
+	}
+
+}

d7b430bff948da7fa8774f08afeee0e7aa4073f4
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-03 20:28:04 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/RemoveDuplicateInteger14.java
@@ -0,0 +1,24 @@
+package codingpactice1;
+
+public class RemoveDuplicateInteger14 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 1, 2, 3, 2, 1 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isduplicate = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isduplicate = true;
+					break;
+				}
+			}
+			if (!isduplicate) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

07bd8130c9b8c1e7eb63e70ff3c3508d166ef5d6
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-03 20:24:34 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/MissingNumber7.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class MissingNumber7 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 4, 5, 6 };
+		int n = arr.length + 1;
+
+		int expectedRes = n * (n + 1) / 2;
+		int actualres = 0;
+
+		for (int i : arr) {
+			actualres += i;
+		}
+		System.out.println(expectedRes - actualres);
+	}
+
+}

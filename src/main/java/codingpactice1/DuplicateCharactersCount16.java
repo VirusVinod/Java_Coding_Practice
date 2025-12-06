efb9f1fe72f52839492ae4cc27e7c1b1e7d2dcc3
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-06 18:51:07 +0530

    Java Code Pratice

---
+++ src/main/java/codingpactice1/DuplicateCharactersCount16.java
@@ -0,0 +1,21 @@
+package codingpactice1;
+
+public class DuplicateCharactersCount16 {
+
+	public static void main(String[] args) {
+
+		String str = "23456785432";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " - " + count[j]);
+			}
+		}
+	}
+
+}

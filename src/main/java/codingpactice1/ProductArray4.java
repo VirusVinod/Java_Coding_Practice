57f985989f8694e248869823c6c7894080ec8c8a
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-07 18:51:50 +0530

    Java Code Pratice

---
+++ src/main/java/codingpactice1/ProductArray4.java
@@ -0,0 +1,16 @@
+package codingpactice1;
+
+public class ProductArray4 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 22, 33 };
+		int count = 1;
+
+		for (int i = 0; i < arr.length; i++) {
+			count = count * arr[i];
+		}
+		System.out.println(count);
+	}
+
+}

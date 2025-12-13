b4ecae22290f040da6c2089dc2b3a70097cedb5c
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-13 09:54:43 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon5.java
+++ src/test/java/SeleniumPractice/Amazon5.java
@@ -2,7 +2,9 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon5 {
@@ -14,7 +16,14 @@ public class Amazon5 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
+//		Step : 02
+		WebElement secarchbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		secarchbox.sendKeys("Iphone");
+
+		WebElement clcikbtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		clcikbtn.click();
+
 	}
 
 }

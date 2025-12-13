275dc5f8ebd7165f9a9a03978f1749ac0d6c8522
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-13 10:14:09 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon5.java
+++ src/test/java/SeleniumPractice/Amazon5.java
@@ -1,11 +1,15 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Amazon5 {
 
@@ -15,7 +19,7 @@ public class Amazon5 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
 //		Step : 02
 		WebElement secarchbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
@@ -23,6 +27,26 @@ public class Amazon5 {
 
 		WebElement clcikbtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		clcikbtn.click();
+
+//		Step : 03
+
+		String expectedRes = "Apple iPhone 15 (128 GB) - Green";
+		List<WebElement> allProducts = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+
+		for (WebElement ele : allProducts) {
+
+			String getallProductName = ele.getText().trim();
+
+			if (getallProductName.contains(expectedRes)) {
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", ele);
+				wait.until(ExpectedConditions.visibilityOf(ele));
+				js.executeScript("arguments[0].click()", ele);
+				break;
+
+			}
+
+		}
 
 	}
 

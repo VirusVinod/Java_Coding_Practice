3abaef0dba4ebfb487a17220cdd42342feb038b0
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-14 18:23:31 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon6.java
+++ src/test/java/SeleniumPractice/Amazon6.java
@@ -1,8 +1,10 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,6 +25,23 @@ public class Amazon6 {
 		seacrchbox.sendKeys("Iphone");
 		WebElement searchbtnclick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		searchbtnclick.click();
+
+//		Step 03
+
+		String expectedRes = "Apple iPhone 15 (128 GB) - Black";
+		List<WebElement> allItem = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+
+		for (WebElement productName : allItem) {
+			String actualRes = productName.getText().trim();
+
+			if (actualRes.contains(expectedRes)) {
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView()", productName);
+				js.executeScript("arguments[0].click()", productName);
+				break;
+
+			}
+		}
 	}
 
 }

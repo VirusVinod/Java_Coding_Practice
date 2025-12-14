f45c77caad78a02016089c400a96e6a98348eda5
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-14 18:33:09 +0530

    pratice

---
+++ src/main/java/codingpactice1/areAnagrams19.java
@@ -0,0 +1,36 @@
+package codingpactice1;
+
+import java.util.Arrays;
+
+public class areAnagrams19 {
+	public static void main(String[] args) {
+
+		String str1 = "code";
+		String str2 = "odec";
+
+		boolean isAnagaram = true;
+
+		if (str1.length() != str2.length()) {
+			isAnagaram = false;
+		} else {
+			char[] ch1 = str1.toCharArray();
+			char[] ch2 = str2.toCharArray();
+
+			Arrays.sort(ch1);
+			;
+			Arrays.sort(ch2);
+
+			if (!Arrays.equals(ch1, ch2)) {
+				isAnagaram = false;
+			}
+		}
+		if (isAnagaram) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+
+		}
+
+	}
+
+}

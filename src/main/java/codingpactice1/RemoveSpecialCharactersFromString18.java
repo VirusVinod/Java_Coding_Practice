96715340185c9ae249ce6c9ca673024e11013944
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-18 18:56:51 +0530

    pratice

---
+++ src/main/java/codingpactice1/RemoveSpecialCharactersFromString18.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class RemoveSpecialCharactersFromString18 {
+
+	public static void main(String[] args) {
+
+		String str = "#$%^&DFGHHGFCVB%$#";
+		String clean = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || ch == ' ') {
+				clean = clean + ch;
+			}
+		}
+		System.out.println(clean);
+	}
+
+}

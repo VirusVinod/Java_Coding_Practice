f79c193c36cabc63200435612c54aa77c55257f1
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-18 19:00:27 +0530

    pratice

---
+++ src/main/java/codingpactice1/SwapTwoString9.java
@@ -0,0 +1,18 @@
+package codingpactice1;
+
+public class SwapTwoString9 {
+
+	public static void main(String[] args) {
+
+		String a = "abc";
+		String b = "xyz";
+
+		a = a + b;
+		b = a.substring(0, a.length() - b.length());
+		a = a.substring(b.length());
+
+		System.out.println("a - " + a);
+		System.out.println("b - " + b);
+	}
+
+}

76ada765b0c487867a7fca374a0e0b3323d3114b
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-19 21:04:27 +0530

    pratice

---
+++ src/main/java/CodingPactice/FirstNonRepeatingChar.java
@@ -0,0 +1,25 @@
+package CodingPactice;
+
+public class FirstNonRepeatingChar {
+
+	public static void main(String[] args) {
+
+		String str = "swiss";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (count[ch] == 1) {
+				System.out.println(ch); 
+				return;
+			}
+		}
+
+		System.out.println("No non-repeating character found");
+	}
+
+}

183d45d7236797d26df64994c4a3c4a4b0c2a083
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-19 20:10:22 +0530

    pratice

---
+++ src/main/java/CodingPactice/ComapretoString2.java
@@ -0,0 +1,14 @@
+package CodingPactice;
+
+public class ComapretoString2 {
+
+	public static void main(String[] args) {
+
+		String s = "Test 1";
+		String S2 = "Test 1";
+
+		System.out.println(s.equals(S2));
+
+	}
+
+}

cef5d4ecbd919460f1b95a0679fdcc6f2ccf0f89
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-24 10:23:05 +0530

    pratice

--- src/test/java/SeleniumPractice/screnshot16.java
+++ src/test/java/SeleniumPractice/screnshot16.java
@@ -1,12 +1,17 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class screnshot16 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 //		Step: 01
 
@@ -14,6 +19,14 @@ public class screnshot16 {
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+//		Step: 02
+
+		File scrneenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File des = new File("abc.png");
+		FileUtils.copyFile(scrneenshot, des);
+
+		driver.quit();
 	}
 
 }

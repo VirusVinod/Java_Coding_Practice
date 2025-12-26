ce93b26be62aa5e73962faa5d20d0d34b48f79dc
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-26 11:58:34 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon10.java
+++ src/test/java/SeleniumPractice/Amazon10.java
@@ -2,8 +2,11 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Amazon10 {
 	
@@ -15,6 +18,16 @@ public class Amazon10 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		
+//		Step 02
+		
+		WebElement searchbox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchbox.sendKeys("iphone");
+		
+		WebElement clicksearchboxbtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		clicksearchboxbtn.click();
 		
 		
 	}

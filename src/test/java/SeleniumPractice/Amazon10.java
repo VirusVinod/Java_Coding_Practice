16197b1222faefe3249121a2af58359eee809a15
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-26 12:17:09 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon10.java
+++ src/test/java/SeleniumPractice/Amazon10.java
@@ -2,6 +2,7 @@ package SeleniumPractice;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -34,7 +35,7 @@ public class Amazon10 {
 
 //		Step 03
 
-		String expectedRes = "iPhone 14 (256 GB) - Starlight";
+		String expectedRes ="iPhone Air 1 TB: Thinnest iPhone Ever, 16.63 cm (6.5″) Display with Promotion up to 120Hz, Powerful A19 Pro Chip, Center Stage Front Camera, All-Day Battery Life; Sky Blue";
 		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
 		for (WebElement product : list) {
 			String ProductName = product.getText().trim();
@@ -44,6 +45,23 @@ public class Amazon10 {
 				js.executeScript("arguments[0].scrollIntoView()", product);
 				wait.until(ExpectedConditions.visibilityOf(product));
 				js.executeScript("arguments[0].click()", product);
+				break;
+			}
+		}
+
+//		Step 04
+
+		String parentId = driver.getWindowHandle();
+		Set<String> allwin = driver.getWindowHandles();
+
+		for (String s : allwin) {
+			if (!parentId.equals(s)) {
+				driver.switchTo().window(s);
+
+				WebElement addtoItemclick = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView()", addtoItemclick);
+				js.executeScript("arguments[0].click()", addtoItemclick);
 				break;
 			}
 		}

1ca8f3370cfaa62482c4483148a3f380830a5241
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-28 10:04:24 +0530

    pratice

---
+++ src/main/java/codingpactice1/ReverseString10.java
@@ -0,0 +1,15 @@
+package codingpactice1;
+
+public class ReverseString10 {
+
+	public static void main(String[] args) {
+
+		String str = "vinod";
+		String res = "";
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + res;
+		}
+		System.out.println(res);
+	}
+
+}

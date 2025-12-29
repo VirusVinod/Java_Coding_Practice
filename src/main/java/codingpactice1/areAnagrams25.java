c18b6152531150a6eccbc18e61d89c3baa000924
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-29 09:46:35 +0530

    pratice

---
+++ src/main/java/codingpactice1/areAnagrams25.java
@@ -0,0 +1,35 @@
+package codingpactice1;
+
+import java.util.Arrays;
+
+public class areAnagrams25 {
+
+	public static void main(String[] args) {
+
+		String str1 = "lappy";
+		String str2 = "apply";
+
+		boolean isAnagram = true;
+
+		if (str1.length() != str2.length()) {
+			isAnagram = false;
+		} else {
+
+			char[] ch1 = str1.toCharArray();
+			char[] ch2 = str2.toCharArray();
+
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+
+			if (!Arrays.equals(ch1, ch2)) {
+				isAnagram = false;
+			}
+		}
+		if (isAnagram) {
+			System.out.println("True");
+		} else {
+			System.out.println("false");
+		}
+	}
+
+}

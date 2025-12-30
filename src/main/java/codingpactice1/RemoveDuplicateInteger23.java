88d12479012558c9d3a969600a3465f7d03fb07a
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-30 09:37:36 +0530

    pratice

---
+++ src/main/java/codingpactice1/RemoveDuplicateInteger23.java
@@ -0,0 +1,20 @@
+package codingpactice1;
+
+public class RemoveDuplicateInteger23 {
+
+	public static void main(String[] args) {
+
+		String str = "QWERTYYTREWQWEREWQ";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.println(res);
+	}
+
+}

e5bb916c18c9533c2b83c5472042b79c66dfb81f
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-30 09:18:39 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon14.java
+++ src/test/java/SeleniumPractice/Amazon14.java
@@ -1,31 +1,51 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon14 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step 01
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		
+
 //		Step 02
-		
+
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone");
-		
-		WebElement searchbtnClick = driver.findElement(By.xpath("nav-search-submit-button"));
+
+		WebElement searchbtnClick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		searchbtnClick.click();
-		
+
+//		Step 03
+
+		String expectedRes = "iPhone Air 512 GB: Thinnest iPhone Ever, 16.63 cm (6.5″) Display with Promotion up to 120Hz, Powerful A19 Pro Chip, Center Stage Front Camera, All-Day Battery Life; Sky Blue";
+		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+
+		for (WebElement product : list) {
+			String actualProd = product.getText().trim();
+
+			if (actualProd.equals(expectedRes)) {
+
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", product);
+				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+		}
+
 	}
 
 }

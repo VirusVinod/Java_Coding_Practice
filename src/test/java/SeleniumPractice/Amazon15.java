3f47086a7f3d81f468252bd3c7ccf89cc86419e7
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-31 09:37:06 +0530

    pratice

---
+++ src/test/java/SeleniumPractice/Amazon15.java
@@ -0,0 +1,20 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon15 {
+	
+	public static void main(String[] args) {
+		
+//		Step: 01
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	}
+
+}

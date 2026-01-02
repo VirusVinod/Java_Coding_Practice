fc38e3a4f4046b7497937e1240a4e1d310f0537a
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-02 09:58:33 +0530

    pratice

---
+++ src/main/java/codingpactice1/RemoveSpecialCharactersFromString23.java
@@ -0,0 +1,20 @@
+package codingpactice1;
+
+public class RemoveSpecialCharactersFromString23 {
+
+	public static void main(String[] args) {
+
+		String str = "WERTYU*&^%$#DFGH&^%";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || ch == ' ') {
+				res = res + ch;
+			}
+		}
+		System.out.println(res);
+	}
+
+}

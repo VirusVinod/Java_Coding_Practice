46c4d9f607b85a4157731edc0510615a71007923
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-03 09:55:22 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon18.java
+++ src/test/java/SeleniumPractice/Amazon18.java
@@ -2,6 +2,7 @@ package SeleniumPractice;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -30,7 +31,7 @@ public class Amazon18 {
 
 //		Step 03
 
-		String expectedRes = "iPhone 16 128 GB: 5G Mobile Phone with Camera Control, A18 Chip and a Big Boost in Battery Life. Works with AirPods; Black";
+		String expectedRes = "iPhone 16e 128 GB: Built for Apple Intelligence, A18 Chip, Supersized Battery Life, 48MP Fusion. Camera, 15.40 cm (6.1″) Super Retina XDR Display; Black";
 		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
 
 		for (WebElement name : list) {
@@ -41,6 +42,23 @@ public class Amazon18 {
 				js.executeScript("arguments[0].scrollIntoView(true)", name);
 				js.executeScript("arguments[0].click()", name);
 				break;
+			}
+		}
+//		Step 04
+
+		String parentId = driver.getWindowHandle();
+		Set<String> alwin = driver.getWindowHandles();
+
+		for (String w : alwin) {
+			if (!parentId.equals(w)) {
+				driver.switchTo().window(w);
+
+				WebElement addtocart = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", addtocart);
+				js.executeScript("arguments[0].click()", addtocart);
+				break;
+
 			}
 		}
 

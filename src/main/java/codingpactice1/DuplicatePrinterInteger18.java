14622767ab11d67d9cef272076be9f669ba60e4d
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-04 11:08:35 +0530

    ptatice

---
+++ src/main/java/codingpactice1/DuplicatePrinterInteger18.java
@@ -0,0 +1,33 @@
+package codingpactice1;
+
+public class DuplicatePrinterInteger18 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 1, 2, 3, 4, 5, 5, 6 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isDuplicate = false;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (isDuplicate) {
+				continue;
+			}
+			for (int k = i + 1; k < n; k++) {
+				if (arr[k] == arr[i]) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (isDuplicate) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

5230b18460fe77841c26b02f846ea9a0328d2224
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-04 10:58:04 +0530

    ptatice

--- src/test/java/SeleniumPractice/Amazon19.java
+++ src/test/java/SeleniumPractice/Amazon19.java
@@ -1,11 +1,16 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -73,6 +78,17 @@ public class Amazon19 {
 				break;
 			}
 		}
+
+//		Step 06
+
+		File screnshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destination = new File("image.png");
+		try {
+			FileUtils.copyFile(screnshot, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }

d9ca8d7117999f73c871a22d5115b8443fa90db2
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-05 10:02:16 +0530

    ptatice

--- src/test/java/SeleniumPractice/Amazon20.java
+++ src/test/java/SeleniumPractice/Amazon20.java
@@ -2,7 +2,9 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon20 {
@@ -15,6 +17,14 @@ public class Amazon20 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+//		Step 02
+		
+		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchItem.sendKeys("iphone");
+		
+		WebElement searchbtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		searchbtn.click();
 	}
 
 }

71fcd29d07f7ca6541a4afa5819749a7f74ee71a
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-05 10:06:23 +0530

    ptatice

--- src/test/java/SeleniumPractice/Amazon20.java
+++ src/test/java/SeleniumPractice/Amazon20.java
@@ -7,24 +7,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon20 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step 01
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
-		
+
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone");
-		
+
 		WebElement searchbtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		searchbtn.click();
+
+//		Step 03
+
+		String expcatedValid = "Results";
+		WebElement getexpactedText = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String actualRes = getexpactedText.getText().trim();
+		Assert.assertEquals(expcatedValid, actualRes);
+		System.out.println("Expected Res = " + expcatedValid);
+		System.out.println("Actual Res = " + actualRes);
+
 	}
 
 }

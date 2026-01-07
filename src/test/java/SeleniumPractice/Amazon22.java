6be0cb72dea428ec1227a2932b82c4b54122351b
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-07 11:10:15 +0530

    pratice code

--- src/test/java/SeleniumPractice/Amazon22.java
+++ src/test/java/SeleniumPractice/Amazon22.java
@@ -1,11 +1,16 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -79,6 +84,16 @@ public class Amazon22 {
 				js.executeScript("arguments[0].click()", addtoCart);
 				break;
 			}
+		}
+
+//		Step 06
+
+		File scrnshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File dest = new File("imagege.jpg");
+		try {
+			FileUtils.copyFile(scrnshot, dest);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}

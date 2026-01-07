f796e1a1a95e95de6632334a285211008456c5d6
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-07 11:17:25 +0530

    pratice code

---
+++ src/main/java/codingpactice1/SwapTwoString11.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class SwapTwoString11 {
+
+	public static void main(String[] args) {
+
+		String a = "Hello";
+		String b = "India";
+
+		a = a + b;
+		b = a.substring(0, a.length() - b.length());
+		a = a.substring(b.length());
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+
+	}
+
+}

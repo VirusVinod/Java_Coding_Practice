8ac24e0aba961f28364353cb64080f191c621bbe
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-09 12:18:03 +0530

    pratice

---
+++ src/main/java/codingpactice1/FirstNonRepeatingChar9.java
@@ -0,0 +1,26 @@
+package codingpactice1;
+
+public class FirstNonRepeatingChar9 {
+
+	public static void main(String[] args) {
+
+		String str = "codedoc";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < str.length(); j++) {
+			char ch = str.charAt(j);
+			if (count[ch] == 1) {
+				System.out.println(ch);
+				return;
+			}
+
+		}
+		System.out.println("No found");
+	}
+
+}

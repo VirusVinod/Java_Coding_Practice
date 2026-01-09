62641d05064a7beb31d213662614fbe651b7b00c
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-09 12:12:25 +0530

    pratice

---
+++ src/main/java/codingpactice1/FibonacciSeries25.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class FibonacciSeries25 {
+
+	public static void main(String[] args) {
+
+		int n = 10, first = 0, second = 1;
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.print(first + ",");
+
+			int next = first + second;
+			first = second;
+			second = next;
+		}
+	}
+
+}

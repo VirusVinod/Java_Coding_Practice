08a50df6a4cc6eb45f15351aff793fcc88700692
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-11 10:15:44 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon26.java
+++ src/test/java/SeleniumPractice/Amazon26.java
@@ -7,25 +7,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon26 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step 01
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
-		
+
 		WebElement search = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		search.sendKeys("iphone");
-		
+
 		WebElement searchBtnClick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		searchBtnClick.click();
-		
+
+//		Step 03
+
+		String ValidateNavigateExp = "Results";
+		WebElement getResul = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String actaulVlue = getResul.getText().trim();
+		Assert.assertEquals(ValidateNavigateExp, actaulVlue);
+		System.out.println("Expected Result : " + ValidateNavigateExp);
+		System.out.println("Actual Result : " + actaulVlue);
+
 	}
 
 }

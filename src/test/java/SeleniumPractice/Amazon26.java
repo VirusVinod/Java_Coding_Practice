d5aabaa654e5b1c05a533d08c25a089d34a2da09
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-11 10:09:08 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon26.java
+++ src/test/java/SeleniumPractice/Amazon26.java
@@ -2,7 +2,9 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon26 {
@@ -12,9 +14,18 @@ public class Amazon26 {
 //		Step 01
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("");
+		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+//		Step 02
+		
+		WebElement search = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		search.sendKeys("iphone");
+		
+		WebElement searchBtnClick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		searchBtnClick.click();
+		
 	}
 
 }

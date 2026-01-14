d24f2ea3394f77880735f48f61493da899429831
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-14 10:34:14 +0530

    pratice

---
+++ src/main/java/codingpactice1/MissingNumber22.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class MissingNumber22 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 4, 5, 7 };
+		int n = arr.length + 1;
+
+		int expectedRes = n * (n + 1) / 2;
+		int actualRes = 0;
+
+		for (int i : arr) {
+			actualRes += i;
+		}
+		System.out.print(expectedRes - actualRes);
+	}
+
+}

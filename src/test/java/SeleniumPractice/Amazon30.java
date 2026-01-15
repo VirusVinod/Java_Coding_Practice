b7f049aa2ea4d81e6b6cec1c140d5fc846823306
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-15 09:34:22 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon30.java
+++ src/test/java/SeleniumPractice/Amazon30.java
@@ -1,8 +1,10 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -33,6 +35,24 @@ public class Amazon30 {
 				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
 		String actualRes = res.getText().trim();
 		Assert.assertEquals(expectedNavigate, actualRes);
+
+//		Step 04
+
+		String expectedProduct = "iPhone 16 Pro Max 256 GB: 5G Mobile Phone with Camera Control, 4K 120 fps Dolby Vision and a Huge Leap in Battery Life. Works with AirPods; Black Titanium";
+		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+
+		for (WebElement product : list) {
+			String actualProduct = product.getText().trim();
+
+			if (actualProduct.equals(expectedProduct)) {
+
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", product);
+				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+		}
 
 	}
 

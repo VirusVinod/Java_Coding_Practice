102935b85b7f8d3e77e5e1a081ead640e48ffbd3
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-17 19:52:02 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon32.java
+++ src/test/java/SeleniumPractice/Amazon32.java
@@ -1,6 +1,5 @@
 package SeleniumPractice;
 
-import java.awt.RenderingHints.Key;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -8,6 +7,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
 
 public class Amazon32 {
 	public static void main(String[] args) {
@@ -18,11 +19,20 @@ public class Amazon32 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
 
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-		searchItem.sendKeys("iphone",Keys.ENTER);
+		searchItem.sendKeys("iphone", Keys.ENTER);
+
+//		Step 03
+
+		String expectedValidated = "Results";
+		WebElement Res = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+
+		String actuaValidated = Res.getText().trim();
+		Assert.assertEquals(expectedValidated, actuaValidated);
 	}
 
 }

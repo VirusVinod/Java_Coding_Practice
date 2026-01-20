0c86c4ead6ea98ad936afb53bffb9fe9fa214609
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-20 10:37:51 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon34.java
+++ src/test/java/SeleniumPractice/Amazon34.java
@@ -3,9 +3,12 @@ package SeleniumPractice;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
 
 public class Amazon34 {
 
@@ -19,8 +22,17 @@ public class Amazon34 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
 //		Step 02
-		
+
 		WebElement searchAndclick = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchAndclick.sendKeys("iphone", Keys.ENTER);
+
+//		Step 03
+
+		String ExpValidated = "Results";
+		WebElement Res = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String actuRes = Res.getText().trim();
+		Assert.assertEquals(ExpValidated, actuRes);
 
 	}
 

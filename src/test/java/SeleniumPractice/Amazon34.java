4d082af5cf7dedf125d613dc6eb4ffdaa8c81bc4
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-20 10:33:10 +0530

    pratice

--- src/test/java/SeleniumPractice/Amazon34.java
+++ src/test/java/SeleniumPractice/Amazon34.java
@@ -2,7 +2,9 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon34 {
@@ -15,6 +17,10 @@ public class Amazon34 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+//		Step 02
+		
+		WebElement searchAndclick = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 
 	}
 

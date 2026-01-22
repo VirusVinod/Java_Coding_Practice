5efa4e553fb3562b06428772d790e470813f4ca5
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-22 14:23:08 +0530

    daliy code pratice

---
+++ src/test/java/SeleniumPractice/Amazon36.java
@@ -0,0 +1,19 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon36 {
+	public static void main(String[] args) {
+
+//		Step 01
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	}
+
+}

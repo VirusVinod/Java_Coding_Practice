666a93c413658fcfc5d601410d049e9cb2ad8244
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-23 11:36:22 +0530

    code pratice

--- src/test/java/SeleniumPractice/Amazon37.java
+++ src/test/java/SeleniumPractice/Amazon37.java
@@ -2,6 +2,7 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,6 +16,12 @@ public class Amazon37 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+//		Step 02
+		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchItem.sendKeys("iphone");
+		WebElement clickBtnSearch = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		clickBtnSearch.click();
 
 	}
 

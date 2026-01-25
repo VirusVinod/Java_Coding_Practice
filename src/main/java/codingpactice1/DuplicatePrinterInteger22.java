0fb9b97f12a4737d109db552e35b7d2965d7c1a8
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-25 10:32:29 +0530

    code pratice

---
+++ src/main/java/codingpactice1/DuplicatePrinterInteger22.java
@@ -0,0 +1,34 @@
+package codingpactice1;
+
+public class DuplicatePrinterInteger22 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 1, 2, 3, 45, 6, 7 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isDuplicate = false;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (isDuplicate) {
+				continue;
+			}
+			for (int k = i + 1; k < n; k++) {
+				if (arr[k] == arr[i]) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (isDuplicate) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

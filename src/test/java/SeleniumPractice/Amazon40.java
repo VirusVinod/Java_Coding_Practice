e2255320f43daf9666f2f5d451657ffeaecbb83b
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-26 20:08:34 +0530

    code pratice

--- src/test/java/SeleniumPractice/Amazon40.java
+++ src/test/java/SeleniumPractice/Amazon40.java
@@ -2,7 +2,10 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon40 {
@@ -15,6 +18,11 @@ public class Amazon40 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+//		Step 02
+
+		WebElement searchItem = driver.findElement(By.xpath(""));
+		searchItem.sendKeys("iphone", Keys.ENTER);
 	}
 
 }

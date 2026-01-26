a28c65183740d6be2a82ce090ff14694f0dd98fe
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-26 20:21:57 +0530

    code pratice

--- src/test/java/SeleniumPractice/Amazon40.java
+++ src/test/java/SeleniumPractice/Amazon40.java
@@ -1,8 +1,10 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,7 +25,7 @@ public class Amazon40 {
 
 //		Step 02
 
-		WebElement searchItem = driver.findElement(By.xpath(""));
+		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone", Keys.ENTER);
 
 //		Step 03
@@ -32,7 +34,25 @@ public class Amazon40 {
 		WebElement result = driver
 				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
 		String expectedResult = result.getText().trim();
-		Assert.assertEquals(actualResult, expectedResult);
+		Assert.assertEquals(expectedResult, actualResult);
+
+//		Step 04
+
+		String expectedProduct = "iPhone 16 Pro Max 1 TB: 5G Mobile Phone with Camera Control, 4K 120 fps Dolby Vision and a Huge Leap in Battery Life. Works with AirPods; Black Titanium";
+		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+		for (WebElement product : list) {
+
+			String actualProduct = product.getText().trim();
+			if (actualProduct.equals(expectedProduct)) {
+
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", product);
+				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+
+		}
 	}
 
 }

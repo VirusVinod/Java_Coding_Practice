14917866be815aa4536c0e72a88c6ec18456cfd8
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-26 20:13:53 +0530

    code pratice

--- src/test/java/SeleniumPractice/Amazon40.java
+++ src/test/java/SeleniumPractice/Amazon40.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon40 {
 
 	public static void main(String[] args) {
@@ -23,6 +25,14 @@ public class Amazon40 {
 
 		WebElement searchItem = driver.findElement(By.xpath(""));
 		searchItem.sendKeys("iphone", Keys.ENTER);
+
+//		Step 03
+
+		String actualResult = "Results";
+		WebElement result = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String expectedResult = result.getText().trim();
+		Assert.assertEquals(actualResult, expectedResult);
 	}
 
 }

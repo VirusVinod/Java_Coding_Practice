b3dc91454b5ae3bf7113a426a244b81b1935d6f9
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-31 11:00:40 +0530

    java selenium code pratice

---
+++ src/main/java/codingpactice1/RemoveDuplicateString30.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class RemoveDuplicateString30 {
+
+	public static void main(String[] args) {
+
+		String str = "ERTYUYTRERTYUOP";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.print(res);
+	}
+
+}

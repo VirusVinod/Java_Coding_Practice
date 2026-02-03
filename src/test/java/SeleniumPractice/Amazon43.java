96aa7195661be496a8e2a257554c1d9fed97ffa8
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-03 09:59:23 +0530

    java selenium code pratice

--- src/test/java/SeleniumPractice/Amazon43.java
+++ src/test/java/SeleniumPractice/Amazon43.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon43 {
 
 	public static void main(String[] args) {
@@ -22,10 +24,17 @@ public class Amazon43 {
 
 		WebElement searcItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searcItem.sendKeys("iphone");
-		
+
 		WebElement searchbtnclick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		searchbtnclick.click();
-		
+
+//		Step 03
+
+		String expectedPage = "Results";
+		WebElement page = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String actualPage = page.getText().trim();
+		Assert.assertEquals(expectedPage, actualPage);
 
 	}
 

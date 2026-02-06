1c38e680f99b1de23a3cf3be4163bbd87519c957
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-06 20:15:25 +0530

    java selenium code practice

--- src/test/java/SeleniumPractice/Amazon44.java
+++ src/test/java/SeleniumPractice/Amazon44.java
@@ -1,28 +1,76 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon44 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step 01
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
-		
+
 		WebElement SearchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-		SearchItem.sendKeys("iphone",Keys.ENTER);
+		SearchItem.sendKeys("iphone", Keys.ENTER);
+
+//		Step 03
+
+		String expactedPageNavigate = "Results";
+		WebElement page = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String actualPageNavigate = page.getText().trim();
+		Assert.assertEquals(expactedPageNavigate, actualPageNavigate);
+
+//		Step 04
+
+		String expectedProduct = "iPhone 17 Pro Max 2 TB: 17.42 cm (6.9″) Display with Promotion, A19 Pro Chip, Best Battery Life in Any iPhone Ever, Pro Fusion Camera System, Center Stage Front Camera; Deep Blue";
+		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+		for (WebElement product : list) {
+
+			String actalProduct = product.getText().trim();
+
+			if (actalProduct.equals(expectedProduct)) {
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", product);
+				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+
+		}
+//		Step 05
+
+		String parentId = driver.getWindowHandle();
+		Set<String> allWin = driver.getWindowHandles();
+
+		for (String w : allWin) {
+			if (!parentId.equals(w)) {
+
+				driver.switchTo().window(w);
+
+				WebElement addToCart = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", addToCart);
+				js.executeScript("arguments[0].click()", addToCart);
+				break;
+			}
+		}
 	}
 
 }

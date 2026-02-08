c1f36ed9ffbd85f37405f5b088a8e2135220e577
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-08 19:43:23 +0530

    java code practice

---
+++ src/main/java/codingpactice1/FirstNonRepeatingChar14.java
@@ -0,0 +1,27 @@
+package codingpactice1;
+
+public class FirstNonRepeatingChar14 {
+
+	public static void main(String[] args) {
+
+		String str = "codedec";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < str.length(); j++) {
+
+			char ch = str.charAt(j);
+			if (count[ch] == 1) {
+				System.out.print(ch);
+				return;
+			}
+		}
+		System.out.println("Not found");
+
+	}
+
+}

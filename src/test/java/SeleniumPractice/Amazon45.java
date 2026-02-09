22f9f8965cbe3f5f3ad47164ae2e0bc7763a466b
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-09 10:34:49 +0530

    Java selenium code pratice

--- src/test/java/SeleniumPractice/Amazon45.java
+++ src/test/java/SeleniumPractice/Amazon45.java
@@ -2,20 +2,28 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon45 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step 01
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
+//		Step 02
+
+		WebElement searchItemandEnter = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchItemandEnter.sendKeys("iphone", Keys.ENTER);
+
 	}
 
 }

00bd1c79d4f5310471e2a29016d9acbff417e3ae
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-10 10:01:10 +0530

    Java selenium code pratice

---
+++ src/main/java/codingpactice1/DuplicateCharactersCount35.java
@@ -0,0 +1,22 @@
+package codingpactice1;
+
+public class DuplicateCharactersCount35 {
+
+	public static void main(String[] args) {
+
+		String str = "EREETYUIIYTRE";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + " - " + count[j]);
+			}
+		}
+	}
+
+}

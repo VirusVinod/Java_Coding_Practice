e1dc4f61cb3751a6297abe7360760cfdfb2fdaa5
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-12 10:01:45 +0530

    java selenium code pratice

--- src/test/java/SeleniumPractice/Amazon47.java
+++ src/test/java/SeleniumPractice/Amazon47.java
@@ -2,6 +2,7 @@ package SeleniumPractice;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -54,6 +55,24 @@ public class Amazon47 {
 				js.executeScript("arguments[0].click()", product);
 				break;
 
+			}
+		}
+
+//		Step 05
+
+		String parenId = driver.getWindowHandle();
+		Set<String> allWin = driver.getWindowHandles();
+
+		for (String w : allWin) {
+			if (!parenId.equals(w)) {
+				driver.switchTo().window(w);
+
+				WebElement addtoCart = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", addtoCart);
+				js.executeScript("arguments[0].click()", addtoCart);
+				break;
 			}
 		}
 

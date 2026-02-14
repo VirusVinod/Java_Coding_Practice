f3dbdbdf47137f3a2309c6be6a4df182fab7a510
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-14 16:20:27 +0530

    java code pratice

--- src/main/java/codingpactice1/DuplicateCharactersCount36.java
+++ src/main/java/codingpactice1/DuplicateCharactersCount36.java
@@ -4,31 +4,19 @@ public class DuplicateCharactersCount36 {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 1, 2, 3, 1, 2, 4 };
-		int n = arr.length;
+		String str = "RTYUIYTDGHYY";
+		int[] count = new int[256];
 
-		for (int i = 0; i < n; i++) {
-			boolean isduplicatePrint = false;
-
-			for (int j = 0; j < i; j++) {
-				if (arr[i] == arr[j]) {
-					isduplicatePrint = true;
-					break;
-				}
-			}
-			if (isduplicatePrint) {
-				continue;
-			}
-			for (int k = i + 1; k < n; k++) {
-				if (arr[i] == arr[k]) {
-					isduplicatePrint = true;
-					break;
-				}
-			}
-			if (isduplicatePrint) {
-				System.out.print(arr[i] + ",");
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+		for (int j = 0; j < 256; j++) {
+			if (count[j] > 1) {
+				System.out.println((char) j + "," + count[j]);
 			}
 		}
+
 	}
 
 }

0e1fce42eee34b8bbd7a0185b3d9c7071052028f
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-16 17:02:04 +0530

    java Selenium code pratice

--- src/test/java/SeleniumPractice/Amazon49.java
+++ src/test/java/SeleniumPractice/Amazon49.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon49 {
 
 	public static void main(String[] args) {
@@ -17,16 +19,23 @@ public class Amazon49 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
-		
+
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone");
-		
+
 		WebElement btnSearchClick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		btnSearchClick.click();
 
-		
+//		Step 03
+
+		String expectedPage = "Results";
+		WebElement page = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String atualPAge = page.getText().trim();
+		Assert.assertEquals(expectedPage, atualPAge);
+
 	}
 
 }

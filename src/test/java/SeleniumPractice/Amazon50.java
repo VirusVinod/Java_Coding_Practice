513b6cad0a4bc145dcedfe87463138fac9ef0cb1
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-18 22:34:26 +0530

    java code pratice

--- src/test/java/SeleniumPractice/Amazon50.java
+++ src/test/java/SeleniumPractice/Amazon50.java
@@ -1,12 +1,17 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -69,6 +74,16 @@ public class Amazon50 {
 				js.executeScript("arguments[0].click()", addToCart);
 				break;
 			}
+		}
+
+//		Step 06
+
+		File screnShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destination = new File("imagess.jpg");
+		try {
+			FileUtils.copyFile(screnShot, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 

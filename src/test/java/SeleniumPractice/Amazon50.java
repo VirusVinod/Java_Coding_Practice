6a65294aa7ae539fac1443a0337c9ccb7ad94ecf
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-18 22:33:25 +0530

    java code pratice

--- src/test/java/SeleniumPractice/Amazon50.java
+++ src/test/java/SeleniumPractice/Amazon50.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon50 {
 
 	public static void main(String[] args) {
@@ -22,6 +24,14 @@ public class Amazon50 {
 
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone", Keys.ENTER);
+
+//		Step 03
+
+		String expectedPage = "Results";
+		WebElement page = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String atualPAge = page.getText().trim();
+		Assert.assertEquals(expectedPage, atualPAge);
 	}
 
 }

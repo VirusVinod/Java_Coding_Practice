687f85c229e9aaefa62d35050ad7e06d0384e867
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-20 18:59:26 +0530

    java code pratice

--- src/test/java/SeleniumPractice/Amazon51.java
+++ src/test/java/SeleniumPractice/Amazon51.java
@@ -2,7 +2,9 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon51 {
@@ -15,6 +17,14 @@ public class Amazon51 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+//		Step 02
+
+		WebElement searcItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searcItem.sendKeys("iphone");
+
+		WebElement BtnsearcItem = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+		BtnsearcItem.click();
 	}
 
 }

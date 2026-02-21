58d305aad2ee86b2f151deec93f225a947ef83e1
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-21 19:10:32 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/ReverseString18.java
@@ -0,0 +1,15 @@
+package codingpactice1;
+
+public class ReverseString18 {
+	public static void main(String[] args) {
+
+		String str = "code";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			res = str.charAt(i) + res;
+		}
+		System.out.println(res);
+	}
+
+}

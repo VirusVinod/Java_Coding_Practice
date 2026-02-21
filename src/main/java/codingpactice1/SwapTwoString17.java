7916df66ca6dc18f8c7e5135e9b2ba3edcb1ba2d
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-21 19:14:57 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/SwapTwoString17.java
@@ -0,0 +1,19 @@
+package codingpactice1;
+
+public class SwapTwoString17 {
+
+	public static void main(String[] args) {
+
+		String a = "A";
+		String b = "B";
+
+		a = a + b;
+		b = a.substring(0, a.length() - b.length());
+		a = a.substring(b.length());
+
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+
+	}
+
+}

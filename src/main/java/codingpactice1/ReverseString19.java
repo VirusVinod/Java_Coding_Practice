c1adb5257a1999d56579dfe5d46c457a6747471b
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-27 17:55:54 +0530

    java code pratice

--- src/main/java/codingpactice1/ReverseString19.java
+++ src/main/java/codingpactice1/ReverseString19.java
@@ -1,17 +1,15 @@
 package codingpactice1;
 
 public class ReverseString19 {
+
 	public static void main(String[] args) {
 
 		String str = "vinoddd";
 		String res = "";
 
 		for (int i = 0; i < str.length(); i++) {
-
 			res = str.charAt(i) + res;
-
 		}
 		System.out.print(res);
 	}
-
 }

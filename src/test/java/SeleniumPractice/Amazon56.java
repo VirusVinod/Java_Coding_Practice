a99b339802155866e6d6ec4ad1a0b08d1e7a5307
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-01 21:39:53 +0530

    java selenium code pratice

--- src/test/java/SeleniumPractice/Amazon56.java
+++ src/test/java/SeleniumPractice/Amazon56.java
@@ -1,8 +1,10 @@
 package SeleniumPractice;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,7 +20,7 @@ public class Amazon56 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
 
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
@@ -26,7 +28,7 @@ public class Amazon56 {
 
 		WebElement BtnCickSearchBox = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		BtnCickSearchBox.click();
-		
+
 //		Step 03
 
 		String ValidatedExpedtedPage = "Results";
@@ -34,6 +36,24 @@ public class Amazon56 {
 				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
 		String ValidActualPage = result.getText().trim();
 		Assert.assertEquals(ValidatedExpedtedPage, ValidActualPage);
+
+//		Step 04
+
+		String expectedProduct = "iPhone 16 Pro Max 1 TB: 5G Mobile Phone with Camera Control, 4K 120 fps Dolby Vision and a Huge Leap in Battery Life. Works with AirPods; White Titanium";
+		List<WebElement> list = driver.findElements(By.xpath("//div[@data-cy='title-recipe']//a//h2"));
+
+		for (WebElement product : list) {
+
+			String actualProduct = product.getText().trim();
+			if (actualProduct.equals(expectedProduct)) {
+
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", product);
+				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+		}
 
 	}
 

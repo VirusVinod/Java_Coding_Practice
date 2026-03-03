ac0fec0c2fd03ffbae58723db074d8f45f8e8c24
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-03 17:29:56 +0530

    java selenium code pratice

--- src/test/java/SeleniumPractice/Amazon58.java
+++ src/test/java/SeleniumPractice/Amazon58.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon58 {
 	public static void main(String[] args) {
 //		Step 01
@@ -16,11 +18,19 @@ public class Amazon58 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
 
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone", Keys.ENTER);
+
+//		Step 03
+
+		String ValidatedExpedtedPage = "Results";
+		WebElement result = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String ValidActualPage = result.getText().trim();
+		Assert.assertEquals(ValidatedExpedtedPage, ValidActualPage);
 
 	}
 

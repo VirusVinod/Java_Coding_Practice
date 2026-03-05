f43ca466ebb1ed444d5cf28d23839583967fb1f4
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-05 22:01:15 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/FirstNonRepeatingChar18.java
@@ -0,0 +1,23 @@
+package codingpactice1;
+
+public class FirstNonRepeatingChar18 {
+	public static void main(String[] args) {
+
+		String str = "pillowpow";
+		int[] count = new int[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+		for (int j = 0; j < str.length(); j++) {
+			char ch = str.charAt(j);
+			if (count[ch] == 1) {
+				System.out.println(ch);
+				return;
+			}
+		}
+		System.out.println("No found");
+	}
+
+}

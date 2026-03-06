9d445c066df93a4fba215288be6d2df9c29234b7
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-06 22:17:36 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/Amazon59.java
+++ src/test/java/SeleniumPractice/Amazon59.java
@@ -1,12 +1,17 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,12 +28,12 @@ public class Amazon59 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
 
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone", Keys.ENTER);
-		
+
 //		Step 03
 
 		String ValidatedExpedtedPage = "Results";
@@ -36,7 +41,7 @@ public class Amazon59 {
 				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
 		String ValidActualPage = result.getText().trim();
 		Assert.assertEquals(ValidatedExpedtedPage, ValidActualPage);
-		
+
 //		Step 04
 
 		String expectedProduct = "iPhone 16 Pro Max 1 TB: 5G Mobile Phone with Camera Control, 4K 120 fps Dolby Vision and a Huge Leap in Battery Life. Works with AirPods; White Titanium";
@@ -54,7 +59,7 @@ public class Amazon59 {
 
 			}
 		}
-		
+
 //		Step 05
 
 		String praentId = driver.getWindowHandle();
@@ -72,6 +77,16 @@ public class Amazon59 {
 				break;
 
 			}
+		}
+
+//		Step 06
+
+		File screnShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destination = new File("abcfg.jpg");
+		try {
+			FileUtils.copyFile(screnShot, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}

da217bdb52a4512c97474258c3b6849d1fec16f7
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-08 22:16:54 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/Amazon60.java
+++ src/test/java/SeleniumPractice/Amazon60.java
@@ -2,6 +2,7 @@ package SeleniumPractice;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -13,21 +14,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import junit.framework.Assert;
 
 public class Amazon60 {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Step 01
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
 
 		WebElement searchItem = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchItem.sendKeys("iphone", Keys.ENTER);
-		
+
 //		Step 03
 
 		String ValidatedExpedtedPage = "Results";
@@ -35,7 +36,7 @@ public class Amazon60 {
 				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
 		String ValidActualPage = result.getText().trim();
 		Assert.assertEquals(ValidatedExpedtedPage, ValidActualPage);
-		
+
 //		Step 04
 
 		String expectedProduct = "iPhone 16 Pro Max 1 TB: 5G Mobile Phone with Camera Control, 4K 120 fps Dolby Vision and a Huge Leap in Battery Life. Works with AirPods; White Titanium";
@@ -49,6 +50,25 @@ public class Amazon60 {
 				JavascriptExecutor js = (JavascriptExecutor) driver;
 				js.executeScript("arguments[0].scrollIntoView(true)", product);
 				js.executeScript("arguments[0].click()", product);
+				break;
+
+			}
+		}
+
+//		Step 05
+
+		String praentId = driver.getWindowHandle();
+		Set<String> allWin = driver.getWindowHandles();
+
+		for (String w : allWin) {
+
+			if (!praentId.equals(w)) {
+				driver.switchTo().window(w);
+
+				WebElement addToCart = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("arguments[0].scrollIntoView(true)", addToCart);
+				js.executeScript("arguments[0].click()", addToCart);
 				break;
 
 			}

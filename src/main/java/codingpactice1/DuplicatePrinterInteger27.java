4986c344021b93696f8ee2e523396a7c5d21dfcc
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-09 16:56:25 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/DuplicatePrinterInteger27.java
@@ -0,0 +1,35 @@
+package codingpactice1;
+
+public class DuplicatePrinterInteger27 {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 4, 3, 2, 5, 7, 5 };
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			boolean isduplicate = false;
+
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j]) {
+					isduplicate = true;
+					break;
+				}
+			}
+			if (isduplicate) {
+				continue;
+			}
+
+			for (int k = i + 1; k < n; k++) {
+				if (arr[k] == arr[i]) {
+					isduplicate = true;
+					break;
+				}
+			}
+			if (isduplicate) {
+				System.out.print(arr[i] + ",");
+			}
+		}
+	}
+
+}

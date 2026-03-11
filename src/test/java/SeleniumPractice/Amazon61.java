6db82a003a794a0d49c4d75405bcbd2d99117b0c
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-11 16:36:06 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/Amazon61.java
+++ src/test/java/SeleniumPractice/Amazon61.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Amazon61 {
 
 	public static void main(String[] args) {
@@ -24,6 +26,14 @@ public class Amazon61 {
 		searchBox.sendKeys("iphone");
 		WebElement searchBtnClick = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
 		searchBtnClick.click();
+
+//		Step 03
+
+		String expactedPage = "Results";
+		WebElement res = driver
+				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
+		String actualPage = res.getText().trim();
+		Assert.assertEquals(expactedPage, actualPage);
 
 	}
 

470a7e3f8769bdbc909682982f2dbc4f9b9097a7
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-13 14:21:54 +0530

    java code pratice

---
+++ src/main/java/codingpactice1/RemoveDuplicateString36.java
@@ -0,0 +1,18 @@
+package codingpactice1;
+
+public class RemoveDuplicateString36 {
+	public static void main(String[] args) {
+
+		String str = "QWERTYUIUYTRQWE";
+		String res = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch) == -1) {
+				res += ch;
+			}
+		}
+		System.out.print(res);
+	}
+
+}

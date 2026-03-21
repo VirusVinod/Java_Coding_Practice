bac4f310f4389eb4435381ceffa730dbbd4c1d1d
vinod singh <vinodkumarsingh680@gmail.com>
2026-03-21 13:42:03 +0530

    Selenium code pratice

---
+++ src/test/java/SeleniumPractice/Amazon69.java
@@ -0,0 +1,27 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon69 {
+	public static void main(String[] args) {
+
+//		Step 01
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+//		Step 02
+
+		WebElement searchAndEnter = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchAndEnter.sendKeys("iphone", Keys.ENTER);
+
+	}
+
+}

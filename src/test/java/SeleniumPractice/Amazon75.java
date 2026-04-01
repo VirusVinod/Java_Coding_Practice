dc9e4010fdf0194ef45c6b2c2d27f109b5479291
vinod singh <vinodkumarsingh680@gmail.com>
2026-04-01 19:31:49 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/Amazon75.java
+++ src/test/java/SeleniumPractice/Amazon75.java
@@ -1,12 +1,17 @@
 package SeleniumPractice;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,18 +20,18 @@ import junit.framework.Assert;
 
 public class Amazon75 {
 	public static void main(String[] args) {
-		
+
 //		Step 01
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 //		Step 02
 
 		WebElement searchAndEnter = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		searchAndEnter.sendKeys("iphone", Keys.ENTER);
-		
+
 //		Step 03
 
 		String expectedPage = "Results";
@@ -34,7 +39,7 @@ public class Amazon75 {
 				.findElement(By.xpath("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']"));
 		String actualPage = page.getText().trim();
 		Assert.assertEquals(expectedPage, actualPage);
-		
+
 //		Step 04
 
 		String expectedProduct = "iPhone 17 Pro 1 TB: 15.93 cm (6.3″) Display with Promotion up to 120Hz, A19 Pro Chip, Breakthrough Battery Life, Pro Fusion Camera System with Center Stage Front Camera; Deep Blue";
@@ -52,7 +57,7 @@ public class Amazon75 {
 
 			}
 		}
-		
+
 //		Step 05
 
 		String praentId = driver.getWindowHandle();
@@ -70,6 +75,16 @@ public class Amazon75 {
 				break;
 
 			}
+		}
+
+//		Step 06
+
+		File screnShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File destination = new File("abcfg.jpg");
+		try {
+			FileUtils.copyFile(screnShot, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}

c30e464965cc3e83a9c359914a5315ace5b43057
vinod singh <vinodkumarsingh680@gmail.com>
2026-04-22 22:09:58 +0530

    selenium code pratice

---
+++ src/test/java/SeleniumPractice/demoqa1.java
@@ -0,0 +1,17 @@
+package SeleniumPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demoqa1 {
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/automation-practice-form");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	}
+
+}

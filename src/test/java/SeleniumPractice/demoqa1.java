7251681f8fd115dff621e8ef42c42581b1c20320
vinod singh <vinodkumarsingh680@gmail.com>
2026-04-23 20:24:58 +0530

    java code pratice

--- src/test/java/SeleniumPractice/demoqa1.java
+++ src/test/java/SeleniumPractice/demoqa1.java
@@ -64,6 +64,18 @@ public class demoqa1 {
 		WebElement stateInput = driver.findElement(By.id("react-select-3-input"));
 		stateInput.sendKeys("NCR");
 		stateInput.sendKeys(Keys.ENTER);
+		
+//		   Select City
+			WebElement cityDropdown = driver.findElement(By.cssSelector("#city div.css-yk16xz-control"));
+			js.executeScript("arguments[0].click()", cityDropdown);
+
+			WebElement cityInput = driver.findElement(By.id("react-select-4-input"));
+			cityInput.sendKeys("Delhi");
+			cityInput.sendKeys(Keys.ENTER);
+			
+			WebElement lgnBtn = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+			lgnBtn.click();
+		}
 	}
 
 }

b98561870cf1220a511cf7ee241287c43a7d40af
vinod singh <vinodkumarsingh680@gmail.com>
2026-04-30 13:14:40 +0530

    Selenium Code Pratice

--- src/test/java/SeleniumPractice/demoqa5.java
+++ src/test/java/SeleniumPractice/demoqa5.java
@@ -2,16 +2,24 @@ package SeleniumPractice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demoqa5 {
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://demoqa.com/automation-practice-form");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		WebElement firstName = driver.findElement(By.id("firstName"));
+		firstName.sendKeys("Test");
+
+		WebElement lastname = driver.findElement(By.id("lastName"));
+		lastname.sendKeys("Test");
 
 	}
 

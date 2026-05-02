c243115b41760d441e6511fe754690ebdd1c1be1
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-02 21:57:04 +0530

    Selenium Code Pratice

--- src/test/java/SeleniumPractice/demoqa5.java
+++ src/test/java/SeleniumPractice/demoqa5.java
@@ -69,6 +69,10 @@ public class demoqa5 {
 		WebElement cityDropdown = driver.findElement(By.cssSelector("#city div.css-yk16xz-control"));
 		js.executeScript("arguments[0].click()", cityDropdown);
 
+		WebElement cityInput = driver.findElement(By.id("react-select-4-input"));
+		cityInput.sendKeys("Delhi");
+		cityInput.sendKeys(Keys.ENTER);
+
 	}
 
 }

38e46b2983f9932b282029a3b970cf404a94d19f
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-04 12:36:48 +0530

    Selenium Code Pratice

--- src/test/java/SeleniumPractice/demoqa6.java
+++ src/test/java/SeleniumPractice/demoqa6.java
@@ -14,19 +14,23 @@ public class demoqa6 {
 		driver.get("https://demoqa.com/automation-practice-form");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 		WebElement firstName = driver.findElement(By.id("firstName"));
 		firstName.sendKeys("Test");
 
 		WebElement lastname = driver.findElement(By.id("lastName"));
 		lastname.sendKeys("Test");
-		
+
 		WebElement maleRadio = driver.findElement(By.id("gender-radio-1"));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", maleRadio);
-		
+
 		WebElement mobileNo = driver.findElement(By.xpath("//input[@id='userNumber']"));
 		mobileNo.sendKeys("9801928281");
+
+//		DatePicker
+		WebElement maleRadatePickerInputdio = driver.findElement(By.id("dateOfBirthInput"));
+		js.executeScript("arguments[0].click()", maleRadatePickerInputdio);
 	}
 
 }

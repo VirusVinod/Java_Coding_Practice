24def30d8759cd1c8c4a4cf4623c6c27e04827cf
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-17 18:23:06 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/SwagLabs3.java
+++ src/test/java/SeleniumPractice/SwagLabs3.java
@@ -16,6 +16,9 @@ public class SwagLabs3 {
 		email.sendKeys("standard_user");
 		WebElement password = driver.findElement(By.id("password"));
 		password.sendKeys("secret_sauce");
+		
+		WebElement loginbtn = driver.findElement(By.id("login-button"));
+		loginbtn.click();
 
 	}
 

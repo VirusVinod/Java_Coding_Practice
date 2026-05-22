cd31e82019e52db1672b5d0bfd5b8af27fb1ca1a
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-22 09:19:07 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/SwagLabs4.java
+++ src/test/java/SeleniumPractice/SwagLabs4.java
@@ -47,6 +47,9 @@ public class SwagLabs4 {
 		
 		WebElement lastName = driver.findElement(By.id("last-name"));
 		lastName.sendKeys("USA Test1");
+		
+		WebElement postalCode = driver.findElement(By.id("postal-code"));
+		postalCode.sendKeys("110095");
 
 	}
 

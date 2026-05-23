47b2ea18ac2b2181b9caa8d0ac02e2e4ccad7547
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-23 21:56:13 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/SwagLabs5.java
+++ src/test/java/SeleniumPractice/SwagLabs5.java
@@ -1,6 +1,8 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SwagLabs5 {
@@ -10,6 +12,8 @@ public class SwagLabs5 {
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
 		
+		WebElement email = driver.findElement(By.id("user-name"));
+		email.sendKeys("standard_user");
 		
 	}
 

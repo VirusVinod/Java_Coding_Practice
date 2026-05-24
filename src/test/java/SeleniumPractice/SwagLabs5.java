8b05202f8f2b07fa3ab2da33dd6b27c8e6bd535c
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-24 12:15:17 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/SwagLabs5.java
+++ src/test/java/SeleniumPractice/SwagLabs5.java
@@ -45,6 +45,9 @@ public class SwagLabs5 {
 		
 		WebElement checkout = driver.findElement(By.id("checkout"));
 		checkout.click();
+		
+		WebElement firstName = driver.findElement(By.id("first-name"));
+		firstName.sendKeys("Test Build11");
 	}
 
 }

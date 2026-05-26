8ab46337c6fac7872943d922f4e4b4ad7018fddf
vinod singh <vinodkumarsingh680@gmail.com>
2026-05-26 17:40:23 +0530

    selenium code pratice

--- src/test/java/SeleniumPractice/SwagLabs6.java
+++ src/test/java/SeleniumPractice/SwagLabs6.java
@@ -15,6 +15,9 @@ public class SwagLabs6 {
 		
 		WebElement email = driver.findElement(By.id("user-name"));
 		email.sendKeys("standard_user");
+		
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("secret_sauce");
 
 	}
 

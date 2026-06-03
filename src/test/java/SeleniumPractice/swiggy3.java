b897653b03afc1fcad5d8a02655220b03afce266
vinod singh <vinodkumarsingh680@gmail.com>
2026-06-03 10:24:52 +0530

    Selenium code pratice

--- src/test/java/SeleniumPractice/swiggy3.java
+++ src/test/java/SeleniumPractice/swiggy3.java
@@ -1,5 +1,7 @@
 package SeleniumPractice;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,15 +13,19 @@ public class swiggy3 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.swiggy.com/");
-		
+
 		WebElement signinClick = driver.findElement(By.xpath("//div[@class='_3chg9']"));
 		signinClick.click();
-		
+
 		WebElement mobileNo = driver.findElement(By.id("mobile"));
 		mobileNo.sendKeys("8802867320");
-		
+
 		WebElement loginButtonclick = driver.findElement(By.xpath("//div[@class='_1cmcE _3R9Dd']"));
 		loginButtonclick.click();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		WebElement otp = driver.findElement(By.id("otp"));
+		otp.sendKeys("000000");
 
 	}
 

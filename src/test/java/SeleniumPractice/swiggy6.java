a4243100bf5a4670b48b13206c7da5aa6df84b5b
vinod singh <vinodkumarsingh680@gmail.com>
2026-06-08 14:33:02 +0530

    selenium

--- src/test/java/SeleniumPractice/swiggy6.java
+++ src/test/java/SeleniumPractice/swiggy6.java
@@ -18,6 +18,9 @@ public class swiggy6 {
 		WebElement mobileNo = driver.findElement(By.id("mobile"));
 		mobileNo.sendKeys("8802867320");
 
+		WebElement loginButtonclick = driver.findElement(By.xpath("//div[@class='_1cmcE _3R9Dd']"));
+		loginButtonclick.click();
+
 	}
 
 }

cf9bcf3cdeb298b99732a512e63e632a2673c3c5
vinod singh <vinodkumarsingh680@gmail.com>
2026-06-11 09:47:31 +0530

    selenium

--- src/test/java/SeleniumPractice/swiggy7.java
+++ src/test/java/SeleniumPractice/swiggy7.java
@@ -13,21 +13,23 @@ public class swiggy7 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.swiggy.com/");
-		
+
 		WebElement signinClick = driver.findElement(By.xpath("//div[@class='_3chg9']"));
 		signinClick.click();
-		
+
 		WebElement mobileNo = driver.findElement(By.id("mobile"));
 		mobileNo.sendKeys("8802867320");
 
-
 		WebElement loginButtonclick = driver.findElement(By.xpath("//div[@class='_1cmcE _3R9Dd']"));
 		loginButtonclick.click();
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		WebElement otp = driver.findElement(By.id("otp"));
 		otp.sendKeys("000000");
 
+		WebElement veryfyOtp = driver.findElement(By.xpath("//div[@class='_1cmcE _3R9Dd']"));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		veryfyOtp.click();
 
 	}
 

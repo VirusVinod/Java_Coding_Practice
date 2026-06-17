cbc3525352312aaa348040c360e0212cc09ecb39
vinod singh <vinodkumarsingh680@gmail.com>
2026-06-17 18:05:28 +0530

    java selenium code

--- src/test/java/SeleniumPractice/swiggy10.java
+++ src/test/java/SeleniumPractice/swiggy10.java
@@ -6,17 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class swiggy10 {
-	
+
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.swiggy.com/");
-		
+
 		WebElement signinClick = driver.findElement(By.xpath("//div[@class='_3chg9']"));
 		signinClick.click();
-		
+
 		WebElement mobileNo = driver.findElement(By.id("mobile"));
 		mobileNo.sendKeys("8802867320");
+
+		WebElement loginButtonclick = driver.findElement(By.xpath("//div[@class='_1cmcE _3R9Dd']"));
+		loginButtonclick.click();
+
 	}
 
 }

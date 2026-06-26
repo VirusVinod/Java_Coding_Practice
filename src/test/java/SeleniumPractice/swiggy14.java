12a54618480e452e6b67e006312b3548000ba6df
vinod singh <vinodkumarsingh680@gmail.com>
2026-06-26 14:27:44 +0530

    java selenium code

--- src/test/java/SeleniumPractice/swiggy14.java
+++ src/test/java/SeleniumPractice/swiggy14.java
@@ -1,6 +1,8 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class swiggy14 {
@@ -8,6 +10,9 @@ public class swiggy14 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.swiggy.com/");
+
+		WebElement signinClick = driver.findElement(By.xpath("//div[@class='_3chg9']"));
+		signinClick.click();
 	}
 
 }

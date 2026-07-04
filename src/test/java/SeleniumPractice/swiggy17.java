0a80694cb272a370c9037a47264965f2be87d20d
vinod singh <vinodkumarsingh680@gmail.com>
2026-07-04 18:16:41 +0530

    selenium java code

---
+++ src/test/java/SeleniumPractice/swiggy17.java
@@ -0,0 +1,15 @@
+package SeleniumPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class swiggy17 {
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.swiggy.com/");
+
+	}
+
+}

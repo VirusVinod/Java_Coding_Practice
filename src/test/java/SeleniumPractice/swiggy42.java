64961bf856f68c5cdf103651b2396bd74c826ee1
vinod singh <vinodkumarsingh680@gmail.com>
2026-09-22 09:45:04 +0530

    java code pratice

--- src/test/java/SeleniumPractice/swiggy42.java
+++ src/test/java/SeleniumPractice/swiggy42.java
@@ -10,10 +10,12 @@ public class swiggy42 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.swiggy.com/");
-		
+
 		WebElement signinClick = driver.findElement(By.xpath("//div[@class='_3chg9']"));
 		signinClick.click();
 
+		WebElement mobileNo = driver.findElement(By.id("mobile"));
+		mobileNo.sendKeys("8802867320");
 	}
 
 }

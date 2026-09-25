a393cb1eff36309d6365a25ec9a6d4b5824b0ecc
vinod singh <vinodkumarsingh680@gmail.com>
2026-09-25 10:29:28 +0530

    java code pratice

--- src/test/java/SeleniumPractice/swiggy43.java
+++ src/test/java/SeleniumPractice/swiggy43.java
@@ -11,12 +11,15 @@ public class swiggy43 {
 		driver.manage().window().maximize();
 		driver.get("https://www.swiggy.com/");
 		driver.manage().window().maximize();
-		
+
 		WebElement signinClick = driver.findElement(By.xpath("//div[@class='_3chg9']"));
 		signinClick.click();
-		
+
 		WebElement mobileNo = driver.findElement(By.id("mobile"));
 		mobileNo.sendKeys("8802867320");
+
+		WebElement loginButtonclick = driver.findElement(By.xpath("//div[@class='_1cmcE _3R9Dd']"));
+		loginButtonclick.click();
 	}
 
 }
